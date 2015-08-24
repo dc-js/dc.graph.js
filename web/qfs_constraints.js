2eebea1dfc4b5fee142e2f8ca7b8b94f6bb01545
Gordon Woodhull <gordon@woodhull.com>
2015-08-24 17:42:54 -0400

    over-constrain the QFS example
    this is very slow but produces a very nice layout
    the code is horrible do not look!

--- web/qfs_constraints.js
+++ web/qfs_constraints.js
@@ -1,4 +1,11 @@
 function qfs_edges(nodes, edges, constraints) {
+    var dids = {
+        CCS: 0,
+        CAV: 0,
+        MAV: 0,
+        MCS: 0,
+        CSAV: 0
+    };
     edges.forEach(function(e) {
         var s = e.source, t = e.target;
         if(s.orig.value.class === 'Client' && t.orig.value.class === 'Metaserver') {
@@ -7,46 +14,113 @@ function qfs_edges(nodes, edges, constraints) {
                 left: e.source.orig.key,
                 right: e.target.orig.key,
                 axis: 'y',
-                gap: 50
+                gap: 100,
+                equality: true
             });
             // this one is just to untangle the diagram by putting metaserver left, client right
             constraints.push({
                 left: e.target.orig.key,
                 right: e.source.orig.key,
                 axis: 'x',
-                gap: 200
+                gap: 700
             });
         }
-        else if(s.orig.value.class === 'Client' && t.orig.value.class === 'ChunkServer') {
+        else if(s.orig.value.class === 'Client') {
+            if(t.orig.value.class === 'ChunkServer' && !dids.CCS++)
+                constraints.push({
+                    left: e.source.orig.key,
+                    right: e.target.orig.key,
+                    axis: 'y',
+                    gap: 200,
+                    equality: true
+                });
+            if(t.orig.value.class === 'Attached Volume' && !dids.CAV++)
+                constraints.push({
+                    left: e.source.orig.key,
+                    right: e.target.orig.key,
+                    axis: 'y',
+                    gap: 300,
+                    equality: true
+                });
+        }
+        else if(s.orig.value.class === 'Metaserver') {
+            if(t.orig.value.class === 'Attached Volume' && !dids.MAV++)
+                constraints.push({
+                    left: e.source.orig.key,
+                    right: e.target.orig.key,
+                    axis: 'y',
+                    gap: 200,
+                    equality: true
+                });
+            if(t.orig.value.class === 'ChunkServer' && !dids.MCS++)
+                constraints.push({
+                    left: e.source.orig.key,
+                    right: e.target.orig.key,
+                    axis: 'y',
+                    gap: 100,
+                    equality: true
+                });
+        }
+        else if(s.orig.value.class === 'ChunkServer' && t.orig.value.class === 'Attached Volume' && !dids.CSAV++) {
             constraints.push({
                 left: e.source.orig.key,
                 right: e.target.orig.key,
                 axis: 'y',
-                gap: 50
+                gap: 100,
+                equality: true
             });
         }
-        else if(s.orig.value.class === 'Metaserver' &&
-                (t.orig.value.class === 'Attached Volume' || t.orig.value.class === 'ChunkServer')) {
+    });
+    var chunks = nodes.filter(function(n) { return n.orig.value.class === 'ChunkServer'; });
+    chunks.sort(function(a, b) {
+        return +a.orig.value.label.slice(2) - +b.orig.value.label.slice(2);
+    });
+    chunks.forEach(function(ch, i) {
+        if(i>0)
             constraints.push({
-                left: e.source.orig.key,
-                right: e.target.orig.key,
-                axis: 'y',
-                gap: 100
+                left: chunks[i-1].orig.key,
+                right: chunks[i].orig.key,
+                axis: 'x',
+                gap: 60
             });
-        }
-        else if(s.orig.value.class === 'ChunkServer' && t.orig.value.class === 'Attached Volume') {
+    });
+    var idex = /^Vol([0-9]+)_([A-Za-z]+)([0-9]+)$/;
+    var chunkvols = nodes.filter(function(n) { return n.orig.value.class === 'Attached Volume'; });
+    chunkvols.sort(function(a, b) {
+        var ma = idex.exec(a.orig.value.name), mb = idex.exec(b.orig.value.name);
+        switch(ma[2]) {
+        case 'Cl':
+            ma[1] = 100;
+            break;
+        case 'MS':
+            ma[1] = -100;
+            break;
+        };
+        switch(mb[2]) {
+        case 'Cl':
+            mb[1] = 100;
+            break;
+        case 'MS':
+            mb[1] = -100;
+            break;
+        };
+        var A = +ma[1]*1000 + +ma[3], B = mb[1]*1000 + +mb[3];
+        return A - B;
+    });
+    chunkvols.forEach(function(ch, i) {
+        if(i>0)
             constraints.push({
-                left: e.source.orig.key,
-                right: e.target.orig.key,
-                axis: 'y',
-                gap: 100
+                left: chunkvols[i-1].orig.key,
+                right: chunkvols[i].orig.key,
+                axis: 'x',
+                gap: 60
             });
-        }
     });
     return constraints;
 }
 
 function qfs_alignment(nodes, edges, constraints) {
+    //return constraints;
     // put ChunkServers and Volumes in same levels
     function level_start() {
         return {
