6750d24424355255f06acab2b99ba4d84c4bc96e
Gordon Woodhull <gordon@woodhull.com>
2015-11-26 10:30:49 -0500

    reformat

--- web/js/qfs_constraints.js
+++ web/js/qfs_constraints.js
@@ -6,47 +6,98 @@ var qfs_constraint_rules = {
         {id: 'Attached Volume', partition: 'class'}
     ],
     edges: [
-        {source: 'Client', target: 'Metaserver', produce: {axis: 'y', gap: 100, equality: true}},
-        {source: 'Client', target: 'Metaserver', reverse: true,
-         produce: function(members) {
-             return {axis: 'x', gap: (members.ChunkServer.nodes.length+1)*60, equality: true};
+        {source: 'Client', target: 'Metaserver',
+         produce: {
+             axis: 'y',
+             gap: 100,
+             equality: true
          }},
-        {source: 'Client', target: 'ChunkServer', produce: {axis: 'y', gap: 200, equality: true}},
-        {source: 'Client', target: 'Attached Volume', produce: {axis: 'y', gap: 300, equality: true}},
-        {source: 'Metaserver', target: 'Attached Volume', produce: {axis: 'y', gap: 200, equality: true}},
-        {source: 'Metaserver', target: 'ChunkServer', produce: {axis: 'y', gap: 100, equality: true}},
-        {source: 'ChunkServer', target: 'Attached Volume', produce: {axis: 'y', gap: 100, equality: true}},
+        {source: 'Client', target: 'Metaserver',
+         reverse: true,
+         produce: function(members) {
+             return {
+                 axis: 'x',
+                 gap: (members.ChunkServer.nodes.length+1)*60,
+                 equality: true
+             };
+         }},
+        {source: 'Client', target: 'ChunkServer',
+         produce: {
+             axis: 'y',
+             gap: 200,
+             equality: true
+         }},
+        {source: 'Client', target: 'Attached Volume',
+         produce: {
+             axis: 'y',
+             gap: 300,
+             equality: true
+         }},
+        {source: 'Metaserver', target: 'Attached Volume',
+         produce: {
+             axis: 'y',
+             gap: 200,
+             equality: true
+         }},
+        {source: 'Metaserver', target: 'ChunkServer',
+         produce: {
+             axis: 'y',
+             gap: 100,
+             equality: true
+         }},
+        {source: 'ChunkServer', target: 'Attached Volume',
+         produce: {
+             axis: 'y',
+             gap: 100,
+             equality: true
+         }},
 
         {source: 'ChunkServer', target: 'ChunkServer',
-         produce: {type: 'alignment', axis: 'y'}, listname: 'offsets',
-         wrap: function(x) { return {node: x, offset: 0}; }},
+         produce: {
+             type: 'alignment',
+             axis: 'y'
+         },
+         listname: 'offsets',
+         wrap: function(x) { return {node: x, offset: 0}; }
+        },
         {source: 'Attached Volume', target: 'Attached Volume',
-         produce: {type: 'alignment', axis: 'y'}, listname: 'offsets',
-         wrap: function(x) { return {node: x, offset: 0}; }},
+         produce: {
+             type: 'alignment',
+             axis: 'y'
+         },
+         listname: 'offsets',
+         wrap: function(x) { return {node: x, offset: 0}; }
+        },
 
         {source: 'ChunkServer', target: 'ChunkServer',
-         produce: {type: 'ordering', axis: 'x', gap: 60,
-                   ordering: function(kv) {
-                       return +kv.value.label.slice(2);
-                   }
-                  }},
+         produce: {
+             type: 'ordering',
+             axis: 'x',
+             gap: 60,
+             ordering: function(kv) {
+                 return +kv.value.label.slice(2);
+             }
+         }},
         {source: 'Attached Volume', target: 'Attached Volume',
-         produce: {type: 'ordering', axis: 'x', gap: 60,
-                   ordering: function(kv) {
-                       if(!this.idex) // is this optimization necessary?
-                           this.idex = /^Vol([0-9]+)_([A-Za-z]+)([0-9]+)$/;
-                       var match = this.idex.exec(kv.value.name);
-                       switch(match[2]) {
-                       case 'Cl':
-                           match[1] = 100;
-                           break;
-                       case 'MS':
-                           match[1] = -100;
-                           break;
-                       };
-                       return +match[1]*1000 + +match[3];
-                   }
-                  }}
+         produce: {
+             type: 'ordering',
+             axis: 'x',
+             gap: 60,
+             ordering: function(kv) {
+                 if(!this.idex) // is this optimization necessary?
+                     this.idex = /^Vol([0-9]+)_([A-Za-z]+)([0-9]+)$/;
+                 var match = this.idex.exec(kv.value.name);
+                 switch(match[2]) {
+                 case 'Cl':
+                     match[1] = 100;
+                     break;
+                 case 'MS':
+                     match[1] = -100;
+                     break;
+                 };
+                 return +match[1]*1000 + +match[3];
+             }
+         }}
     ]
 };
 
