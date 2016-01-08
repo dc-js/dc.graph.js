86614db71678bbf2fcdd098e4b4d401b58feceaa
Gordon Woodhull <gordon@woodhull.com>
2016-01-08 09:42:28 -0500

    comment

--- web/js/flat-group.js
+++ web/js/flat-group.js
@@ -1,6 +1,6 @@
-// when there will be exactly one or zero items in a group, a reasonable reduction
-// is just to use the row or null
 var flat_group = (function() {
+    // when there will be exactly one or zero items in a group, a reasonable reduction
+    // is just to use the row or null
     function one_zero_reduce(group) {
         group.reduce(
             function(p, v) { return v; },
@@ -8,6 +8,7 @@ var flat_group = (function() {
             function() { return null; }
         );
     }
+    // now we only really want to see the non-null values, so make a fake group
     function non_null(group) {
         return {
             all: function() {
