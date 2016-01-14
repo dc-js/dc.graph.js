454ed53fa25057cf0c9b1af586ae838be671d7e0
Gordon Woodhull <gordon@woodhull.com>
2016-01-14 16:36:11 -0500

    Math.hypot polyfill
    for IE - it's still quite slow and the svg markers for the arrowheads
    don't show up, but it seems worthwhile to have diagrams sort of show
    anyway.

--- src/core.js
+++ src/core.js
@@ -92,3 +92,17 @@ function generate_path(pts, bezness, close) {
         cats.push('Z');
     return cats.join('');
 }
+
+// for IE (do we care really?)
+Math.hypot = Math.hypot || function() {
+  var y = 0;
+  var length = arguments.length;
+
+  for (var i = 0; i < length; i++) {
+    if (arguments[i] === Infinity || arguments[i] === -Infinity) {
+      return Infinity;
+    }
+    y += arguments[i] * arguments[i];
+  }
+  return Math.sqrt(y);
+};
