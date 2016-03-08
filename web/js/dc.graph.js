bad0987c0841a9a318015875cdc7bbe7ca0da8e9
Gordon Woodhull <gordon@woodhull.com>
2016-03-08 11:58:41 -0500

    artifacts

--- web/js/dc.graph.js
+++ web/js/dc.graph.js
@@ -1,7 +1,20 @@
 /*!
  *  dc.graph 0.1.1
  *  http://dc-js.github.io/dc.graph.js/
- *  Copyright (c) 2016 AT&T Intellectual Property
+ *  Copyright 2015-2016 AT&T Intellectual Property & the dc.graph.js Developers
+ *  https://github.com/dc-js/dc.graph.js/blob/master/AUTHORS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  */
 (function() { function _dc_graph(d3, crossfilter, dc) {
