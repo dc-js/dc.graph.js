d3be30c3f4b59fbe18ee2d443fbfc324acc01d39
Gordon Woodhull <gordon@woodhull.com>
2016-04-05 12:08:47 -0400

    simplify vfc layout and specify aesthetics

--- web/js/vfc_layout.js
+++ web/js/vfc_layout.js
@@ -10,10 +10,20 @@ app_layouts.vfc = {
             {source: 'VM', target: 'Host', produce: dc_graph.gap_y(100, true)},
 
             {source: 'VNF', target: 'VNF', produce: dc_graph.align_y()},
+/*
             {source: 'VFC', target: 'VFC', produce: dc_graph.align_y()},
             {source: 'VM', target: 'VM', produce: dc_graph.align_y()},
-            {source: 'Host', target: 'Host', produce: dc_graph.align_y()}
+            {source: 'Host', target: 'Host', produce: dc_graph.align_y()}*/
         ]
+    },
+    initDiagram: function(diagram) {
+        diagram
+            .nodeLabel(null)
+            .nodeRadius(3)
+            .parallelEdgeOffset(1)
+            .edgeArrowSize(0.5)
+            .nodeTitle(function(n) { return n.value.name; })
+        ;
     }
 };
 
