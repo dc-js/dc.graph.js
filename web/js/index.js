387b246918913a3ae220d67111dee34585a5add6
Gordon Woodhull <gordon@woodhull.com>
2016-04-19 08:00:36 -0400

    iterate over paths
    this is not at all general, just part of nepal demo

--- web/js/index.js
+++ web/js/index.js
@@ -20,6 +20,7 @@ var steptime = +querystring.interval || 1000, // ms per step
     transition = querystring.transition || 0,
     stage = querystring.stage || 'insmod',
     file = querystring.file || null,
+    paths = querystring.paths || null,
     generate = querystring.gen || null,
     shape = querystring.shape || null,
     radius = +querystring.radius || 25,
@@ -385,6 +386,50 @@ source(function(error, data) {
                           apply_expander_filter();
                           run();
                       }));
+    }
+
+    if(paths) {
+        // make sure it draws first (?)
+        setTimeout(function() {
+            d3.json(paths, function(error, pathv) {
+                if(error)
+                    throw new Error(error);
+                var i = 0;
+                setInterval(function() {
+                    // i continue not to understand the horrible concurrency issues
+                    // i'm running into - double-draws can peg the CPU
+                    if(diagram.isRunning())
+                        return;
+                    var path = pathv.results[i].element_list;
+                    var pnodes = {}, pedges = {};
+                    path.forEach(function(el) {
+                        switch(el.element_type) {
+                        case 'node':
+                            pnodes[el.property_map.ecomp_uid] = true;
+                            break;
+                        case 'edge':
+                            pedges[el.property_map.source_ecomp_uid + '-' + el.property_map.target_ecomp_uid] = true;
+                            break;
+                        }
+                    });
+                    diagram
+                        .edgeStrokeWidth(function(e) {
+                            return pedges[diagram.edgeKey()(e)] ? 4 : 1;
+                        })
+                        .edgeStroke(function(e) {
+                            return pedges[diagram.edgeKey()(e)] ? 'red' : 'black';
+                        })
+                        .nodeStrokeWidth(function(n) {
+                            return pnodes[diagram.nodeKey()(n)] ? 3 : 1;
+                        })
+                        .nodeStroke(function(n) {
+                            return pnodes[diagram.nodeKey()(n)] ? 'red' : 'black';
+                        })
+                        .redraw();
+                    i = (i+1) % pathv.results.length;
+                }, 2000);
+            });
+        }, 1000);
     }
 
     // respond to browser resize (not necessary if width/height is static)
