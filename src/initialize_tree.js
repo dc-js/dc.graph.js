f0fb2d9db3e9c1bfd0504f7da32ff04de3f295ca
Gordon Woodhull <gordon@woodhull.com>
2016-04-18 20:02:29 -0400

    initialize_tree xgap

--- src/initialize_tree.js
+++ src/initialize_tree.js
@@ -1,11 +1,11 @@
 // this naive tree-drawer is paraphrased from memory from dot
-dc_graph.initialize_tree = function(rootf, treef, gap) {
+dc_graph.initialize_tree = function(rootf, treef, xgap, ygap) {
     var x = 0;
     var dfs = dc_graph.depth_first_traversal(rootf, treef, function(n, r) {
         n.cola.x = x;
-        n.cola.y = r*gap;
+        n.cola.y = r*ygap;
     }, function() {
-        x += 12;
+        x += xgap;
     });
 
     return dfs;
