49092d18f3909bed6259159042988da6470d056c
Gordon Woodhull <gordon@woodhull.com>
2016-04-18 23:00:26 -0400

    balance tree coordinates in x
    but this is tangled again

--- src/tree_constraints.js
+++ src/tree_constraints.js
@@ -4,21 +4,22 @@ dc_graph.tree_constraints = function(rootf, treef, xgap, ygap) {
         var constraints = [];
         var x = 0;
         var dfs = dc_graph.depth_first_traversal(rootf, treef, function(n, r, row) {
-            if(row.length) {
-                var last = row[row.length-1];
-                constraints.push({
-                    left: param(diagram.nodeKey())(last),
-                    right: param(diagram.nodeKey())(n),
-                    axis: 'x',
-                    gap: x-last.foo_x,
-                    equality: true
-                });
-            }
-            n.foo_x = x;
+            n.left_x = x;
             // n.cola.x = x;
             // n.cola.y = r*ygap;
         }, function() {
             x += xgap;
+        }, null, null, function(n, row) {
+            var last = row[row.length-1], right = x;
+            if(row.length) {
+                constraints.push({
+                    left: param(diagram.nodeKey())(last),
+                    right: param(diagram.nodeKey())(n),
+                    axis: 'x',
+                    gap: (n.left_x + right - 2*last.left_x)/2,
+                    equality: true
+                });
+            }
         });
         dfs(diagram, nodes, edges);
         return constraints;
