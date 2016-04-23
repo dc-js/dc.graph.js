e260d896235068107cba6bcc56ff27c61f925823
Gordon Woodhull <gordon@woodhull.com>
2016-04-22 21:04:00 -0400

    rename initialize_tree -> tree_positions

--- src/tree_positions.js
+++ src/tree_positions.js
@@ -1,5 +1,5 @@
 // this naive tree-drawer is paraphrased from memory from dot
-dc_graph.initialize_tree = function(rootf, rowf, treef, ofsx, ofsy, xgap, ygap) {
+dc_graph.tree_positions = function(rootf, rowf, treef, ofsx, ofsy, xgap, ygap) {
     var x = ofsx;
     var dfs = dc_graph.depth_first_traversal(rootf, rowf, treef, function(n, r) {
         n.left_x = x;
