afac7faf556f846861ef54216020235eb20614c1
Gordon Woodhull <gordon@woodhull.com>
2016-04-22 20:46:15 -0400

    rolling average of in-edge tail positions
    for a better head position

--- src/initialize_tree.js
+++ src/initialize_tree.js
@@ -3,11 +3,18 @@ dc_graph.initialize_tree = function(rootf, rowf, treef, ofsx, ofsy, xgap, ygap) 
     var x = ofsx;
     var dfs = dc_graph.depth_first_traversal(rootf, rowf, treef, function(n, r) {
         n.left_x = x;
+        n.hit_ins = 1;
         n.cola.y = r*ygap + ofsy;
     }, function() {
         x += xgap;
     }, null, function(n) {
         n.cola.x = (n.left_x + x)/2;
+        delete n.left_x;
+    }, function(n, indegree) {
+        // rolling average of in-neighbor x positions
+        n.cola.x = (n.hit_ins*n.cola.x + x)/++n.hit_ins;
+        if(n.hit_ins === indegree)
+            delete n.hit_ins;
     });
 
     return dfs;
