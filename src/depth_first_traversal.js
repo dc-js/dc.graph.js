6e1d964614df206d5628f1a5974118f07428c2ee
Gordon Woodhull <gordon@woodhull.com>
2016-04-22 20:32:06 -0400

    dfs find roots
    and parameterize row

--- src/depth_first_traversal.js
+++ src/depth_first_traversal.js
@@ -1,14 +1,15 @@
 // this naive tree-drawer is paraphrased from memory from dot
-dc_graph.depth_first_traversal = function(rootf, treef, placef, sibf, pushf, popf) {
+dc_graph.depth_first_traversal = function(rootf, rowf, treef, placef, sibf, pushf, popf) {
     return function(diagram, nodes, edges) {
         if(treef)
             edges = edges.filter(function(e) { return treef(e.orig); });
-
+        var indegree = {};
         var outmap = edges.reduce(function(m, e) {
             var tail = param(diagram.edgeSource())(e),
                 head = param(diagram.edgeTarget())(e);
             if(!m[tail]) m[tail] = [];
             m[tail].push(e);
+            indegree[head] = (indegree[head] || 0) + 1;
             return m;
         }, {});
 
@@ -37,13 +38,13 @@ dc_graph.depth_first_traversal = function(rootf, treef, placef, sibf, pushf, pop
         if(rootf)
             roots = nodes.filter(function(n) { return rootf(n.orig); });
         else {
-            throw new Error("root-finder not implemented (it's easy!)");
+            roots = nodes.filter(function(n) { return !indegree[param(diagram.nodeKey())(n)]; });
         }
         roots.forEach(function(n, ni) {
             if(ni && sibf)
                 sibf();
             pushf && pushf();
-            place_tree(n, 0);
+            place_tree(n, rowf ? rowf(n.orig) : 0);
         });
     };
 };
