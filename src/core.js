1629822ce090b906407fffc3333949559df60180
Gordon Woodhull <gordon@woodhull.com>
2016-04-24 17:52:39 -0400

    property predicates should be typed

--- src/core.js
+++ src/core.js
@@ -74,15 +74,24 @@ var property = function (defaultValue, unwrap) {
 };
 
 // i'm sure there's a word for this in haskell
-function conditional_properties(pred, props) {
+function conditional_properties(npred, epred, props) {
     function _if(pred, curr) {
         return function(o, last) {
             return pred(o) ? curr(o) : last();
         };
     }
     var props2 = {};
-    for(var p in props)
-        props2[p] = _if(pred, param(props[p]));
+    for(var p in props) {
+        if(/^node/.test(p)) {
+            if(npred)
+                props2[p] = _if(npred, param(props[p]));
+        }
+        else if(/^edge/.test(p)) {
+            if(epred)
+                props2[p] = _if(epred, param(props[p]));
+        }
+        else console.error('only know how to deal with properties that start with "node" or "edge"');
+    }
     return props2;
 }
 
