715c9746e6c75422408903adab27b8b51b3bfd48
Gordon Woodhull <gordon@woodhull.com>
2016-05-10 09:31:14 -0400

    make last graph munging backward compatible

--- web/js/munge-graph.js
+++ web/js/munge-graph.js
@@ -23,13 +23,9 @@ function munge_graph(data, nodekeyattr, sourceattr, targetattr) {
 
     //var edgekeyattr = "id";
     var edge0 = edges[0];
-    if(sourceattr && !edge0[sourceattr])
-        throw new Error('sourceattr ' + sa + " didn't work");
-    if(targetattr && !edge0[targetattr])
-        throw new Error('targetattr ' + ta + " didn't work");
     if(edge0[sa] === undefined) {
-        var sourceattrs = ['source_ecomp_uid', "node1", "source", "tail"],
-            targetattrs = ['target_ecomp_uid', "node2", "target", "head"];
+        var sourceattrs = sourceattr ? [sourceattr] : ['source_ecomp_uid', "node1", "source", "tail"],
+            targetattrs = targetattr ? [targetattr] : ['target_ecomp_uid', "node2", "target", "head"];
         //var edgekeyattrs = ['id', '_id', 'ecomp_uid'];
         var edgewrappers = ['edge'];
         if(edge0.node0 && edge0.node1) { // specific conflict here
@@ -37,11 +33,17 @@ function munge_graph(data, nodekeyattr, sourceattr, targetattr) {
             ta = 'node1';
         }
         else {
-            var candidates = sourceattr ? [sourceattr] : find_attr(edge0, sourceattrs);
+            var candidates = find_attr(edge0, sourceattrs);
             if(!candidates.length) {
-                wi = edgewrappers.findIndex(function(w) { return edge0[w] && find_attr(edge0[w], candidates).length; });
-                if(wi<0)
-                    throw new Error("didn't find any source attr");
+                wi = edgewrappers.findIndex(function(w) {
+                    return edge0[w] && find_attr(edge0[w], sourceattrs).length;
+                });
+                if(wi<0) {
+                    if(sourceattr)
+                        throw new Error('sourceattr ' + sa + " didn't work");
+                    else
+                        throw new Error("didn't find any source attr");
+                }
                 edges = edges.map(function(e) { return e[edgewrappers[wi]]; });
                 edge0 = edges[0];
                 candidates = find_attr(edge0, sourceattrs);
@@ -50,9 +52,13 @@ function munge_graph(data, nodekeyattr, sourceattr, targetattr) {
                 console.warn('found more than one possible source attr', candidates);
             sa = candidates[0];
 
-            candidates = targetattr ? [targetattr] : find_attr(edge0, targetattrs);
-            if(!candidates.length)
-                throw new Error("didn't find any target attr");
+            candidates = find_attr(edge0, targetattrs);
+            if(!candidates.length) {
+                if(targetattr && !edge0[targetattr])
+                    throw new Error('targetattr ' + ta + " didn't work");
+                else
+                    throw new Error("didn't find any target attr");
+            }
             if(candidates.length > 1)
                 console.warn('found more than one possible target attr', candidates);
             ta = candidates[0];
@@ -69,16 +75,20 @@ function munge_graph(data, nodekeyattr, sourceattr, targetattr) {
         }
     }
     var node0 = nodes[0];
-    if(nodekeyattr && !node0[nodekeyattr])
-        throw new Error('nodekeyattr ' + nka + " didn't work");
     if(node0[nka] === undefined) {
-        var nodekeyattrs = ['ecomp_uid', 'id', '_id'];
+        var nodekeyattrs = nodekeyattr ? [nodekeyattr] : ['ecomp_uid', 'id', '_id'];
         var nodewrappers = ['vertex'];
-        candidates = nodekeyattr ? [nodekeyattr] : find_attr(node0, nodekeyattrs);
+        candidates = find_attr(node0, nodekeyattrs);
         if(!candidates.length) {
-            wi = nodewrappers.findIndex(function(w) { return node0[w] && find_attr(node0[w], candidates).length; });
-            if(wi<0)
-                throw new Error("couldn't find the node data");
+            wi = nodewrappers.findIndex(function(w) {
+                return node0[w] && find_attr(node0[w], nodekeyattrs).length;
+            });
+            if(wi<0) {
+                if(nodekeyattr)
+                    throw new Error('nodekeyattr ' + nka + " didn't work");
+                else
+                    throw new Error("couldn't find the node data");
+            }
             nodes = nodes.map(function(n) { return n[nodewrappers[wi]]; });
             node0 = nodes[0];
             candidates = find_attr(node0, nodekeyattrs);
