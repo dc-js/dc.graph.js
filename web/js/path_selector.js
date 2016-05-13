b7e34519891b7a7575f2935a7692e2dd3cefdd76
Gordon Woodhull <gordon@woodhull.com>
2016-05-13 16:34:59 -0400

    path_selector hover and click behaviors

--- web/js/path_selector.js
+++ web/js/path_selector.js
@@ -3,14 +3,33 @@ function path_selector(parent, reader, pathsgroup) {
     var root = d3.select(parent);
     var hovered = null, selected = null;
 
+    // unfortunately these functions are copied from dc_graph.highlight_paths
     function contains_path(paths) {
         return function(path) {
             return paths ? paths.indexOf(path)>=0 : false;
         };
     }
 
+    function doesnt_contain_path(paths) {
+        var cp = contains_path(paths);
+        return function(path) {
+            return !cp(path);
+        };
+    }
+
+    function toggle_paths(pathsA, pathsB) {
+        if(!pathsA)
+            return pathsB;
+        else if(!pathsB)
+            return pathsA;
+        if(pathsB.every(contains_path(pathsA)))
+            return pathsA.filter(doesnt_contain_path(pathsB));
+        else return pathsA.concat(pathsB.filter(doesnt_contain_path(pathsA)));
+    }
+
     // this should use the whole cascading architecture
     // and allow customization rather than hardcoding everything
+    // in fact, you can't even reliably overlap attributes without that (so we don't)
     highlight_paths_group
         .on('paths_changed.selector', function(nop, eop, paths) {
             var p2 = root.selectAll('p').data(paths);
@@ -26,7 +45,16 @@ function path_selector(parent, reader, pathsgroup) {
             p2.exit().transition(1000).attr('opacity', 0).remove();
             p2.select('span').text(function(d, i) {
                 return 'path ' + (i+1) + ' (' + reader.elementList.eval(d).length + ')';
-            });
+            })
+                .on('mouseover', function(d) {
+                    highlight_paths_group.hover_changed([d]);
+                })
+                .on('mouseout', function(d) {
+                    highlight_paths_group.hover_changed(null);
+                })
+                .on('click', function(d) {
+                    highlight_paths_group.select_changed(toggle_paths(selected, [d]));
+                });
         })
         .on('hover_changed.selector', function(hpaths) {
             hovered = hpaths;
