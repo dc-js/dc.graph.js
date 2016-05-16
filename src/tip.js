6657e3ee3f19bc0d653ec15aa6c93ef5209ed353
Gordon Woodhull <gordon@woodhull.com>
2016-05-16 04:14:20 -0400

    fix/improve documentation for dc_graph.tip
    update to JSDoc
    add some notes

--- src/tip.js
+++ src/tip.js
@@ -1,10 +1,28 @@
-/* asynchronous d3.tip support for dc.graph.js (optional) */
+/**
+ * Asynchronous [d3.tip](https://github.com/Caged/d3-tip) support for dc.graph.js
+ *
+ * Add tooltips to the nodes and edges of a graph using an asynchronous callback to get
+ * the html to show.
+ *
+ * Optional - requires separately loading the d3.tip script and CSS (which are included in
+ * dc.graph.js in `web/js/d3-tip/index.js` and `web/css/d3-tip/example-styles.css`)
+ *
+ * @name tip
+ * @memberof dc_graph
+ * @return {Object}
+ **/
 dc_graph.tip = function() {
     var _tip = {}, _d3tip = null;
 
     /**
-     #### .parent([object])
-     Assigns this tip object to a diagram. It will show tips for nodes in that diagram.
+     * Assigns this tip object to a diagram. It will show tips for nodes in that diagram.
+     * Usually you will not call this function directly. Instead, attach the tip object
+     * using `diagram.child('tip', dc_graph.tip())`
+     * @name parent
+     * @memberof dc_graph.tip
+     * @instance
+     * @param {dc_graph.diagram} [parent]
+     * @return {dc_graph.diagram}
      **/
     _tip.parent = property(null)
         .react(function(p) {
@@ -44,9 +62,18 @@ dc_graph.tip = function() {
     }
 
     /**
-     #### .table()
-     Generates a handler which can be passed to `tip.content` which produces a table
-     of the attributes and values of the hovered object.
+     * Generates a handler which can be passed to `tip.content` to produce a table of the
+     * attributes and values of the hovered object.
+     *
+     * Note: this interface is not great and is subject to change in the near term.
+     * @name table
+     * @memberof dc_graph.tip
+     * @instance
+     * @return {Function}
+     * @example
+     * // show all the attributes and values in the node and edge objects
+     * var tip = dc_graph.tip();
+     * tip.content(tip.table());
      **/
     _tip.table = function() {
         return function(d, k) {
@@ -62,11 +89,20 @@ dc_graph.tip = function() {
     };
 
     /**
-     #### .content([function])
-     Specifies the function to generate content for the tooltip. This function has
-     the signature `function(d, k)`, where `d` is the datum of the node being hovered over,
-     and `k` is a continuation. The function should fetch the content, asynchronously
-     if needed, and then pass html forward to `k`.
+     * Specifies the function to generate content for the tooltip. This function has the
+     * signature `function(d, k)`, where `d` is the datum of the node being hovered over,
+     * and `k` is a continuation. The function should fetch the content, asynchronously if
+     * needed, and then pass html forward to `k`.
+     * @name content
+     * @memberof dc_graph.tip
+     * @instance
+     * @param {Function} [content]
+     * @return {Function}
+     * @example
+     * // Default behavior: show title
+     * var tip = dc_graph.tip().content(function(d, k) {
+     *     k(_tip.parent() ? _tip.parent().nodeTitle.eval(d) : '');
+     * });
      **/
     _tip.content = property(function(d, k) {
         k(_tip.parent() ? _tip.parent().nodeTitle.eval(d) : '');
