46c2b9f1203685bad8abea877e692974675fbc99
Gordon Woodhull <gordon@woodhull.com>
2016-05-16 03:45:36 -0400

    edge tips and table tips

--- src/tip.js
+++ src/tip.js
@@ -9,14 +9,22 @@ dc_graph.tip = function() {
     _tip.parent = property(null)
         .react(function(p) {
             if(p)
-                p.on('drawn.tip', function(node, edge) {
-                    annotate(node);
+                p.on('drawn.tip', function(node, edge, ehover) {
+                    annotate(node, ehover);
                 });
             else if(_tip.parent())
                 _tip.parent().on('drawn.tip', null);
         });
 
-    function annotate(node) {
+    function fetch_and_show_content(fetcher) {
+         return function(d) {
+             var target = d3.event.target;
+             _tip[fetcher]()(d, function(content) {
+                 _d3tip.show(content, target);
+             });
+         };
+    }
+    function annotate(node, ehover) {
         if(!_d3tip) {
             _d3tip = d3.tip()
                 .attr('class', 'd3-tip')
@@ -24,23 +32,41 @@ dc_graph.tip = function() {
             _tip.parent().svg().call(_d3tip);
         }
         node
-            .on('mouseover.tip', function(d) {
-                var target = d3.event.target;
-                _tip.content()(d, function(content) {
-                    _d3tip.show(content, target);
-                });
-            })
-	    .on('mouseout.foo', function(d) {
-		_d3tip.hide();
-	    });
+            .on('mouseover.tip', fetch_and_show_content('content'))
+            .on('mouseout.tip', function(d) {
+                _d3tip.hide();
+            });
+        ehover
+            .on('mouseover.tip', fetch_and_show_content('content'))
+            .on('mouseout.tip', function(d) {
+                _d3tip.hide();
+            });
     }
+
+    /**
+     #### .table()
+     Generates a handler which can be passed to `tip.content` which produces a table
+     of the attributes and values of the hovered object.
+     **/
+    _tip.table = function() {
+        return function(d, k) {
+            d = d.orig.value;
+            var keys = Object.keys(d);
+            var table = d3.select(document.createElement('table'));
+            var rows = table.selectAll('tr').data(keys);
+            var rowsEnter = rows.enter().append('tr');
+            rowsEnter.append('td').text(function(k) { return k; });
+            rowsEnter.append('td').text(function(k) { return d[k]; });
+            k(table.node().outerHTML); // optimizing for clarity over speed (?)
+        };
+    };
 
     /**
      #### .content([function])
      Specifies the function to generate content for the tooltip. This function has
      the signature `function(d, k)`, where `d` is the datum of the node being hovered over,
      and `k` is a continuation. The function should fetch the content, asynchronously
-     if needed, and then pass it forward to `k`.
+     if needed, and then pass html forward to `k`.
      **/
     _tip.content = property(function(d, k) {
         k(_tip.parent() ? _tip.parent().nodeTitle.eval(d) : '');
