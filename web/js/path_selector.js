ce179c1ef16ec3d89bcd29b2b9ccb41cee4c0488
Gordon Woodhull <gordon@woodhull.com>
2016-05-19 05:10:49 -0400

    remove select & hover when paths change

--- web/js/path_selector.js
+++ web/js/path_selector.js
@@ -30,53 +30,69 @@ function path_selector(parent, reader, pathsgroup) {
     // this should use the whole cascading architecture
     // and allow customization rather than hardcoding everything
     // in fact, you can't even reliably overlap attributes without that (so we don't)
+
+    function draw_paths(paths) {
+        var p2 = root.selectAll('span').data(paths);
+        p2.enter()
+            .append('span')
+            .attr('class', 'path-selector')
+            .style({
+                'border-width': '1px',
+                'border-style': 'solid',
+                'border-color': 'grey',
+                'border-radius': '4px',
+                'display': 'inline-block',
+                padding: '4px',
+                cursor: 'pointer',
+                margin: '5px'
+            });
+        p2.exit().transition(1000).attr('opacity', 0).remove();
+        p2.text(function(d, i) {
+            return 'path ' + (i+1) + ' (' + reader.elementList.eval(d).length + ')';
+        })
+            .on('mouseover', function(d) {
+                highlight_paths_group.hover_changed([d]);
+            })
+            .on('mouseout', function(d) {
+                highlight_paths_group.hover_changed(null);
+            })
+            .on('click', function(d) {
+                highlight_paths_group.select_changed(toggle_paths(selected, [d]));
+            });
+    }
+
+    function draw_hovered() {
+        var is_hovered = contains_path(hovered);
+        root.selectAll('span.path-selector')
+            .style({
+                'border-color': function(d, i) { return is_hovered(d) ? '#e41a1c' : 'grey'; },
+                'border-width': function(d, i) { return (is_hovered(d) ? 2 : 1) + 'px'; },
+                padding: function(d, i) { return (is_hovered(d) ? 3 : 4) + 'px'; }
+            });
+    }
+
+    function draw_selected() {
+        var is_selected = contains_path(selected);
+        root.selectAll('span.path-selector')
+            .style({
+                'background-color': function(d, i) { return is_selected(d) ? '#1c1ae6' : 'white'; },
+                'color': function(d, i) { return is_selected(d) ? 'white' : 'black'; }
+            });
+    }
+
     highlight_paths_group
         .on('paths_changed.selector', function(nop, eop, paths) {
-            var p2 = root.selectAll('span').data(paths);
-            p2.enter()
-              .append('span')
-                .attr('class', 'path-selector')
-                .style({
-                    'border-width': '1px',
-                    'border-style': 'solid',
-                    'border-color': 'grey',
-                    'border-radius': '4px',
-                    'display': 'inline-block',
-                    padding: '4px',
-                    cursor: 'pointer',
-                    margin: '5px'
-                });
-            p2.exit().transition(1000).attr('opacity', 0).remove();
-            p2.text(function(d, i) {
-                return 'path ' + (i+1) + ' (' + reader.elementList.eval(d).length + ')';
-            })
-                .on('mouseover', function(d) {
-                    highlight_paths_group.hover_changed([d]);
-                })
-                .on('mouseout', function(d) {
-                    highlight_paths_group.hover_changed(null);
-                })
-                .on('click', function(d) {
-                    highlight_paths_group.select_changed(toggle_paths(selected, [d]));
-                });
+            hovered = selected = null;
+            draw_paths(paths);
+            draw_hovered();
+            draw_selected();
         })
         .on('hover_changed.selector', function(hpaths) {
             hovered = hpaths;
-            var is_hovered = contains_path(hovered);
-            root.selectAll('span.path-selector')
-                .style({
-                    'border-color': function(d, i) { return is_hovered(d) ? '#e41a1c' : 'grey'; },
-                    'border-width': function(d, i) { return (is_hovered(d) ? 2 : 1) + 'px'; },
-                    padding: function(d, i) { return (is_hovered(d) ? 3 : 4) + 'px'; }
-                });
+            draw_hovered();
         })
         .on('select_changed.selector', function(spaths) {
             selected = spaths;
-            var is_selected = contains_path(selected);
-            root.selectAll('span.path-selector')
-                .style({
-                    'background-color': function(d, i) { return is_selected(d) ? '#1c1ae6' : 'white'; },
-                    'color': function(d, i) { return is_selected(d) ? 'white' : 'black'; }
-                });
+            draw_selected();
         });
 }
