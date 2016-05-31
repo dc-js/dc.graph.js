d6e61500387127005ee4f9acbb8a27403879643a
Gordon Woodhull <gordon@woodhull.com>
2016-05-31 13:34:58 -0400

    use uuids for interior nodes in convert_nest
    bland, inscrutible, simple

--- src/core.js
+++ src/core.js
@@ -140,6 +140,14 @@ function clone(obj) {
     return target;
 }
 
+// http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
+
 // because i don't think we need to bind edge point data (yet!)
 var bez_cmds = {
     1: 'L', 2: 'Q', 3: 'C'
