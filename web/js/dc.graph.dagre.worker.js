0dd44aee45bf1a4c4c11e456f86c89cbdbbdda58
Gordon Woodhull <gordon@woodhull.com>
2016-10-18 23:45:10 -0400

    artifacts

--- web/js/dc.graph.dagre.worker.js
+++ web/js/dc.graph.dagre.worker.js
@@ -1,5 +1,5 @@
 /*!
- *  dc.graph 0.3.11
+ *  dc.graph 0.3.12
  *  http://dc-js.github.io/dc.graph.js/
  *  Copyright 2015-2016 AT&T Intellectual Property & the dc.graph.js Developers
  *  https://github.com/dc-js/dc.graph.js/blob/master/AUTHORS
