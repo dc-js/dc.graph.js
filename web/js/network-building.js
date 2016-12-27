5b4d47554594c8eb41934a15faeeea8becb5fd59
Gordon Woodhull <gordon@woodhull.com>
2016-12-27 15:11:57 -0500

    start network builder
    draw some nodes

---
+++ web/js/network-building.js
@@ -0,0 +1,30 @@
+var qs = querystring.parse();
+
+var node_flat = dc_graph.flat_group.make([], function(d) { return d.id; }),
+    edge_flat = dc_graph.flat_group.make([], function(d) { return d.source + '-' + d.target; });
+
+var diagram = dc_graph.diagram('#graph');
+
+diagram
+    .width(window.innerWidth)
+    .height(window.innerHeight)
+    .layoutAlgorithm('cola')
+    .transitionDuration(500)
+    .stageTransitions('insmod')
+    .showLayoutSteps(false)
+    .nodeDimension(node_flat.dimension).nodeGroup(node_flat.group)
+    .edgeDimension(edge_flat.dimension).edgeGroup(edge_flat.group)
+    .edgeSource(function(e) { return e.value.source; })
+    .edgeTarget(function(e) { return e.value.target; })
+    .nodeLabel(function(n) { return n.key; })
+    .edgeArrowhead('vee');
+
+
+var draw_graphs = dc_graph.draw_graphs({
+    nodeCrossfilter: node_flat.crossfilter,
+    edgeCrossfilter: edge_flat.crossfilter
+});
+
+diagram.child('draw-graphs', draw_graphs);
+
+dc.renderAll();
