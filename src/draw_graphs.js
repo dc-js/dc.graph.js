f0d81ccc2cd2338f8e00275042c47901eae02a04
Gordon Woodhull <gordon@woodhull.com>
2016-12-27 16:11:22 -0500

    position nodes and label (blank for now)

--- src/draw_graphs.js
+++ src/draw_graphs.js
@@ -5,7 +5,9 @@ dc_graph.draw_graphs = function(options) {
         throw new Error('need edgeCrossfilter');
     var idTag = options.idTag || 'id',
         sourceTag = options.sourceTag || 'source',
-        targetTag = options.targetTag || 'target';
+        targetTag = options.targetTag || 'target',
+        labelTag = options.labelTag || 'label',
+        fixedPosTag = options.fixedPosTag || 'fixedPos';
 
     var source = null;
 
@@ -15,9 +17,13 @@ dc_graph.draw_graphs = function(options) {
                 source = d;
             });
         chart.svg().on('click.draw-node', function() {
-            var node = {
-                id: uuid()
-            };
+            var node = {};
+            node[idTag] = uuid();
+            node[labelTag] = '';
+            var bound = chart.root().node().getBoundingClientRect();
+            var coords = chart.invertCoord([d3.event.clientX - bound.left,
+                                            d3.event.clientY - bound.top]);
+            node[fixedPosTag] = {x: coords[0], y: coords[1]};
             options.nodeCrossfilter.add([node]);
             chart.redraw();
         }).on('mousemove.draw-edge', function() {
