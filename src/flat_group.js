cfb84ec03bfe3543e8ec9ccc9ffcd65b89906a34
Gordon Woodhull <gordon@woodhull.com>
2017-02-12 16:52:59 -0500

    document flat_group

--- src/flat_group.js
+++ src/flat_group.js
@@ -1,7 +1,35 @@
-/* for the special case where there will be exactly one or zero items in a group,
- a reasonable reduction is just to use the row or null.
- this could be useful outside dc.graph (esp e.g bubble charts, scatter plots where each
- observation is either shown or not) but it would have to be cleaned up a bit */
+/**
+ * `dc_graph.flat_group` implements a special ["fake group"](https://github.com/dc-js/dc.js/wiki/FAQ#fake-groups)
+ * for the special case where you want a group that represents the filtered rows of the crossfilter.
+ *
+ * Although `dc_graph` can be used with reduced data, typically the nodes and edges are just rows of
+ * the corresponding data arrays, and each array has a column which contains the unique identifier
+ * for the node or edge. In this setup, there are other dimensions and groups which are aggregated
+ * for the use of dc.js charts, but the graph just shows or does not show the nodes and edges from
+ * the rows.
+ *
+ * This simple class supports that use case in three steps:
+ *  1. It creates a dimension keyed on the unique identifier (specified to `flat_group.make`)
+ *  2. It creates a group from the dimension with a reduction function that returns the row when the
+ *  row is filtered in, and `null` when the row is filtered out.
+ *  3. It wraps the group in a fake group which filters out the resulting nulls.
+ *
+ * The result is a fake group whose `.all()` method returns an array of the currently filtered-in
+ * `{key, value}` pairs, where the key is that returned by the ID accessor, and the value is the raw
+ * row object from the data.
+ *
+ * This could be a useful crossfilter utility outside of dc.graph. For example, bubble charts and
+ * scatter plots often use similar functionality because each observation is either shown or not,
+ * and it is helpful to have the entire row available as reduced data.
+ *
+ * But it would need to be generalized and cleaned up. (For example, the way it has to create the
+ * crossfilter and dimension is kinda dumb.) And there is currently no such crossfilter utility
+ * library to put it in.
+ *
+ * @namespace flat_group
+ * @memberof dc_graph
+ * @type {{}}
+**/
 
 dc_graph.flat_group = (function() {
     var reduce_01 = {
@@ -32,11 +60,34 @@ dc_graph.flat_group = (function() {
     }
 
     return {
+        /**
+         * Create a crossfilter, dimension, and flat group, as described in {@link dc_graph.flat_group flat_group}.
+         * Returns an object containing all three.
+
+         * @method make
+         * @memberof dc_graph.flat_group
+         * @param {Array} vec - the data array for crossfilter
+         * @param {Function} id_accessor - accessor function taking a row object and returning its
+         * unique identifier
+         * @return {Object} `{crossfilter, dimension, group}`
+         **/
         make: function(vec, id_accessor) {
             var ndx = crossfilter(vec);
             return dim_group(ndx, id_accessor);
         },
-        another: function(ndx, id_accessor) { // wretched name
+        /**
+         * Create a flat dimension and group from an existing crossfilter.
+         *
+         * This is a wretched name for this function.
+
+         * @method another
+         * @memberof dc_graph.flat_group
+         * @param {Object} ndx - crossfilter instance
+         * @param {Function} id_accessor - accessor function taking a row object and returning its
+         * unique identifier
+         * @return {Object} `{crossfilter, dimension, group}`
+         **/
+        another: function(ndx, id_accessor) {
             return dim_group(ndx, id_accessor);
         }
     };
