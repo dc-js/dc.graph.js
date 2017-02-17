77d0c965544a46fa9064e3efa79be737e8d93480
Gordon Woodhull <gordon@woodhull.com>
2017-02-17 17:46:41 -0500

    start on a tree layout
    to replace tree_positions, eliminate initialLayout & initialOnly, etc.

--- src/depth_first_traversal.js
+++ src/depth_first_traversal.js
@@ -4,25 +4,30 @@
 
 // this is an argument for providing a graph API which could make it
 // easy to just write a recursive function instead of using this
-dc_graph.depth_first_traversal = function(callbacks) { // {init, root, row, tree, place, sib, push, pop, skip, finish}
-    return function(diagram, nodes, edges) {
+dc_graph.depth_first_traversal = function(callbacks) { // {init, root, row, tree, place, sib, push, pop, skip, finish, nodeid, sourceid, targetid}
+    return function(nodes, edges) {
         callbacks.init && callbacks.init();
         if(callbacks.tree)
-            edges = edges.filter(function(e) { return callbacks.tree(e.orig); });
+            edges = edges.filter(function(e) { return callbacks.tree(e); });
         var indegree = {};
         var outmap = edges.reduce(function(m, e) {
-            var tail = diagram.edgeSource.eval(e),
-                head = diagram.edgeTarget.eval(e);
+            var tail = callbacks.sourceid(e),
+                head = callbacks.targetid(e);
             if(!m[tail]) m[tail] = [];
             m[tail].push(e);
             indegree[head] = (indegree[head] || 0) + 1;
+            return m;
+        }, {});
+        var nmap = nodes.reduce(function(m, n) {
+            var key = callbacks.nodeid(n);
+            m[key] = n;
             return m;
         }, {});
 
         var rows = [];
         var placed = {};
         function place_tree(n, r) {
-            var key = diagram.nodeKey.eval(n);
+            var key = callbacks.nodeid(n);
             if(placed[key]) {
                 callbacks.skip && callbacks.skip(n, indegree[key]);
                 return;
@@ -34,10 +39,11 @@ dc_graph.depth_first_traversal = function(callbacks) { // {init, root, row, tree
             placed[key] = true;
             if(outmap[key])
                 outmap[key].forEach(function(e, ei) {
+                    var target = nmap[callbacks.targetid(e)];
                     if(ei && callbacks.sib)
-                        callbacks.sib(false, outmap[key][ei-1].target, e.target);
+                        callbacks.sib(false, outmap[key][ei-1].target, target);
                     callbacks.push && callbacks.push();
-                    place_tree(e.target, r+1);
+                    place_tree(target, r+1);
                 });
             callbacks.pop && callbacks.pop(n);
         }
@@ -46,7 +52,7 @@ dc_graph.depth_first_traversal = function(callbacks) { // {init, root, row, tree
         if(callbacks.root)
             roots = nodes.filter(function(n) { return callbacks.root(n.orig); });
         else {
-            roots = nodes.filter(function(n) { return !indegree[diagram.nodeKey.eval(n)]; });
+            roots = nodes.filter(function(n) { return !indegree[callbacks.nodeid(n)]; });
         }
         roots.forEach(function(n, ni) {
             if(ni && callbacks.sib)
---
+++ src/tree_layout.js
@@ -0,0 +1,159 @@
+/**
+ * `dc_graph.tree_layout` is a very simple and not very bright tree layout. It can draw any DAG, but
+ * tries to position the nodes as a tree.
+ * @class tree_layout
+ * @memberof dc_graph
+ * @param {String} [id=uuid()] - Unique identifier
+ * @return {dc_graph.tree_layout}
+ **/
+dc_graph.tree_layout = function(id) {
+    var _layoutId = id || uuid();
+    var _dispatch = d3.dispatch('tick', 'start', 'end');
+    var _dfs;
+
+    function init(options) {
+        var x;
+        var nwidth = d3.functor(options.nwidth);
+        function best_dist(left, right) {
+            return (nwidth(left) + nwidth(right)) / 2;
+        }
+        _dfs = dc_graph.depth_first_traversal({
+            nodeid: function(n) {
+                return n.dcg_nodeKey;
+            },
+            sourceid: function(n) {
+                return n.dcg_edgeSource;
+            },
+            targetid: function(n) {
+                return n.dcg_edgeTarget;
+            },
+            init: function() {
+                x = options.offsetX;
+            },
+            root: options.rootFunction,
+            row: options.rowFunction,
+            tree: options.treeFunction,
+            place: function(n, r, row) {
+                if(row.length) {
+                    var left = row[row.length-1];
+                    var g = (nwidth(left) + nwidth(n)) / 2;
+                    x = Math.max(x, left.left_x + g);
+                }
+                n.left_x = x;
+                n.hit_ins = 1;
+                n.y = r*options.gapY + options.offsetY;
+            },
+            sib: function(isroot, left, right) {
+                var g = best_dist(left, right);
+                if(isroot) g = g*1.5;
+                x += g;
+            },
+            pop: function(n) {
+                n.x = (n.left_x + x)/2;
+            },
+            skip: function(n, indegree) {
+                // rolling average of in-neighbor x positions
+                n.x = (n.hit_ins*n.x + x)/++n.hit_ins;
+                if(n.hit_ins === indegree)
+                    delete n.hit_ins;
+            },
+            finish: function(rows) {
+                // this is disgusting. patch up any places where nodes overlap by scanning
+                // right far enough to find the space, then fill from left to right at the
+                // minimum gap
+                rows.forEach(function(row) {
+                    var sort = row.sort(function(a, b) { return a.x - b.x; });
+                    var badi = null, badl = null, want;
+                    for(var i=0; i<sort.length-1; ++i) {
+                        var left = sort[i], right = sort[i+1];
+                        if(!badi) {
+                            if(right.x - left.x < best_dist(left, right)) {
+                                badi = i;
+                                badl = left.x;
+                                want = best_dist(left, right);
+                            } // else still not bad
+                        } else {
+                            want += best_dist(left, right);
+                            if(i < sort.length - 2 && right.x < badl + want)
+                                continue; // still bad
+                            else {
+                                if(badi>0)
+                                    --badi; // might want to use more left
+                                var l, limit;
+                                if(i < sort.length - 2) { // found space before right
+                                    var extra = right.x - (badl + want);
+                                    l = sort[badi].x + extra/2;
+                                    limit = i+1;
+                                } else {
+                                    l = Math.max(sort[badi].x, badl - best_dist(sort[badi], sort[badi+1]) - (want - right.x + badl)/2);
+                                    limit = sort.length;
+                                }
+                                for(var j = badi+1; j<limit; ++j) {
+                                    l += best_dist(sort[j-1], sort[j]);
+                                    sort[j].x = l;
+                                }
+                                badi = badl = want = null;
+                            }
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    var _nodes, _edges;
+    function data(nodes, edges, constraints, options) {
+        _nodes = nodes;
+        _edges = edges;
+    }
+
+    function start(options) {
+        _dfs(_nodes, _edges);
+        _dispatch.end(_nodes, _edges);
+    }
+
+    function stop() {
+    }
+
+    var layout = {
+        layoutAlgorithm: function() {
+            return 'tree';
+        },
+        layoutId: function() {
+            return _layoutId;
+        },
+        on: function(event, f) {
+            _dispatch.on(event, f);
+            return this;
+        },
+        init: function(options) {
+            this.optionNames().forEach(function(option) {
+                options[option] = options[option] || this[option]();
+            }.bind(this));
+            init(options);
+            return this;
+        },
+        data: function(nodes, edges, constraints, opts) {
+            data(nodes, edges, constraints, opts);
+        },
+        start: function(options) {
+            start(options);
+        },
+        stop: function() {
+            stop();
+        },
+        optionNames: function() {
+            return ['offsetX', 'offsetY', 'rootFunction', 'rowFunction', 'treeFunction', 'rootFunction', 'gapY'];
+        },
+        offsetX: property(10),
+        offsetY: property(20),
+        rootFunction: property(null),
+        rowFunction: property(null),
+        treeFunction: property(null),
+        rootFunction: property(null),
+        gapY: property(100)
+    };
+    return layout;
+}
+
+dc_graph.tree_layout.scripts = [];
