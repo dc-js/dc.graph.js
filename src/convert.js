17196bb84fb162f87b5347b18296212c99f3b328
Gordon Woodhull <gordon@woodhull.com>
2017-03-15 12:45:55 -0400

    a slightly more general convert_nest

--- src/convert.js
+++ src/convert.js
@@ -1,39 +1,63 @@
-// make crossfilter-suitable data from d3.nest {key, values} format
-dc_graph.convert_nest = function(nest, attrs, nodeKeyAttr, edgeSourceAttr, edgeTargetAttr, parent, inherit) {
-    inherit = inherit || {};
-    var level = Object.keys(inherit).length;
+var convert_nest_helper = function(data, attrs, options, parent, level, inherit) {
+    level = level || 0;
     if(attrs.length) {
         var attr = attrs.shift();
         var nodes = [], edges = [];
-        var children = nest.map(function(v) {
-            inherit[attr] = v.key;
-            var child = uuid();
-            var node = clone(inherit);
-            node[nodeKeyAttr] = child;
-            node.name = attr + ':' + v.key;
-            node._level = level+1;
+        var children = data.map(function(v) {
+            var key = v[options.nestKey];
+            var childKey = options.nestKeysUnique ? key : uuid();
+            var node;
+            if(options.ancestorKeys) {
+                inherit = inherit || {};
+                if(attr)
+                    inherit[attr] = key;
+                node = Object.assign({}, inherit);
+            } else node = {};
+            node._level = level;
+            node[options.nodeKey] = childKey;
+            if(options.label && options.labelFun)
+                node[options.label] = options.labelFun(key, attr, v);
+            if(options.level)
+                node[options.level] = level+1;
             nodes.push(node);
             if(parent) {
                 var edge = {};
-                edge[edgeSourceAttr] = parent;
-                edge[edgeTargetAttr] = child;
+                edge[options.edgeSource] = parent;
+                edge[options.edgeTarget] = childKey;
                 edges.push(edge);
             }
-            var recurse = dc_graph.convert_nest(v.values, attrs.slice(0), nodeKeyAttr, edgeSourceAttr, edgeTargetAttr, child, clone(inherit));
+            var recurse = convert_nest_helper(v.values, attrs.slice(0), options,
+                                              childKey, level+1, Object.assign({}, inherit));
             return recurse;
         });
         return {nodes: Array.prototype.concat.apply(nodes, children.map(dc.pluck('nodes'))),
                 edges: Array.prototype.concat.apply(edges, children.map(dc.pluck('edges')))};
     }
-    else return {nodes: nest.map(function(v) {
+    else return {nodes: data.map(function(v) {
         v._level = level+1;
         return v;
-    }), edges: nest.map(function(v) {
+    }), edges: data.map(function(v) {
         var edge = {};
-        edge[edgeSourceAttr] = parent;
-        edge[edgeTargetAttr] = v[nodeKeyAttr];
+        edge[options.edgeSource] = parent;
+        edge[options.edgeTarget] = v[options.nodeKey];
         return edge;
     })};
+};
+
+dc_graph.convert_nest = function(data, attrs, options) {
+    options = Object.assign({
+        nodeKey: 'key',
+        edgeKey: 'key',
+        edgeSource: 'sourcename',
+        edgeTarget: 'targetname',
+        nestKey: 'key'
+    }, options);
+    if(Array.isArray(data))
+        return convert_nest_helper(data, attrs, options);
+    else {
+        attrs = [''].concat(attrs);
+        return convert_nest_helper([data], attrs, options);
+    }
 };
 
 dc_graph.convert_adjacency_list = function(nodes, namesIn, namesOut) {
