2600da8d2f88972a1ee48ce200c580d6487cb6cb
Gordon Woodhull <gordon@woodhull.com>
2017-03-20 17:05:57 -0400

    attempt to update dimension filter from selection
    didn't work?

---
+++ src/filter_selection.js
@@ -0,0 +1,23 @@
+dc_graph.filter_selection = function() {
+    var select_nodes_group = dc_graph.select_nodes_group('select-nodes-group');
+
+    function selection_changed_listener(chart) {
+        return function(selection) {
+            console.log(selection);
+            if(selection.length) {
+                var set = d3.set(selection);
+                chart.nodeDimension().filterFunction(function(k) {
+                    return set.has(k);
+                });
+            } else chart.nodeDimension().filter(null);
+            chart.redrawGroup();
+        };
+    }
+
+    var _behavior = {
+        parent: property(null).react(function(p) {
+            select_nodes_group.on('node_set_changed.filter-selection', p ? selection_changed_listener(p) : null);
+        })
+    };
+    return _behavior;
+};
