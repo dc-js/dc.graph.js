dc-js/dc.graph.js

commit ff0fbe194bb50bc58d9b5dab0284367f40f583ba

edgeIsShown
here, to hide vnf-vnf edges in hierarchy

src/diagram.js
@@ -573,6 +573,10 @@ dc_graph.diagram = function (parent, chartGroup) {
         return !kv.value.notLayout;
     });
 
+    // conversely, you could have an edge but not want to draw it - not documenting this
+    // yet because it seems like it maybe should be combined with edgeIsLayout
+    _chart.edgeIsShown = _chart.edgeIsLayoutAccessor = property(true);
+
     /**
      * Currently, three strategies are supported for specifying the lengths of edges:
      * * 'individual' - uses the `edgeLength` for each edge. If it returns falsy, uses the
@@ -1107,6 +1111,8 @@ dc_graph.diagram = function (parent, chartGroup) {
 
         // remove self-edges (since we can't draw them - will be option later)
         wedges = wedges.filter(function(e) { return e.source !== e.target; });
+
+        wedges = wedges.filter(_chart.edgeIsShown.eval);
 
         // and optionally, nodes that have no edges
         if(_chart.induceNodes()) {

commit e430bbba35b5c32bc1398a500163244da4c6843c

eliminate paste bug introduced in ff0fbe194bb5
edgeIsLayout and edgeIsShown are confounded enough already
also, improve comment - previously it looked like these would be laid
out but not shown. in fact edgeIsShown(false) means it's neither shown
nor laid out

src/diagram.js
@@ -613,9 +613,10 @@ dc_graph.diagram = function (parent, chartGroup) {
         return !kv.value.notLayout;
     });
 
-    // conversely, you could have an edge but not want to draw it - not documenting this
-    // yet because it seems like it maybe should be combined with edgeIsLayout
-    _chart.edgeIsShown = _chart.edgeIsLayoutAccessor = property(true);
+    // if false, don't draw or layout the edge. this is not documented because it seems like
+    // the interface could be better and this combined with edgeIsLayout. (currently there is
+    // no way to layout but not draw an edge.)
+    _chart.edgeIsShown = property(true);
 
     /**
      * Currently, three strategies are supported for specifying the lengths of edges: