81803c38aae6fa5aabbe4018e1de19da306891ac
Gordon Woodhull <gordon@woodhull.com>
2017-04-28 14:57:25 -0400

    psv and csv support in load_graph

--- src/load_graph.js
+++ src/load_graph.js
@@ -1,3 +1,57 @@
+function process_dot(callback, error, text) {
+    if(error) {
+        callback(error, null);
+        return;
+    }
+    var digraph = graphlibDot.parse(text);
+
+    var nodeNames = digraph.nodes();
+    var nodes = new Array(nodeNames.length);
+    nodeNames.forEach(function (name, i) {
+        var node = nodes[i] = digraph._nodes[nodeNames[i]];
+        node.id = i;
+        node.name = name;
+    });
+
+    var edgeNames = digraph.edges();
+    var edges = [];
+    edgeNames.forEach(function(e) {
+        var edge = digraph._edges[e];
+        edges.push({
+            source: digraph._nodes[edge.u].id,
+            target: digraph._nodes[edge.v].id,
+            sourcename: edge.u,
+            targetname: edge.v
+        });
+    });
+    var graph = {nodes: nodes, links: edges};
+    callback(null, graph);
+}
+
+function process_dsv(callback, error, data) {
+    if(error) {
+        callback(error, null);
+        return;
+    }
+    var keys = Object.keys(data[0]);
+    var source = keys[0], target = keys[1];
+    var nodes = d3.set(data.map(function(r) { return r[source]; }));
+    data.forEach(function(r) {
+        nodes.add(r[target]);
+    });
+    nodes = nodes.values().map(function(k) { return {name: k}; });
+    callback(null, {
+        nodes: nodes,
+        links: data.map(function(r, i) {
+            return {
+                key: i,
+                sourcename: r[source],
+                targetname: r[target]
+            };
+        })
+    });
+}
+
 // load a graph from various formats and return the data in consistent {nodes, links} format
 dc_graph.load_graph = function() {
     // ignore any query parameters for checking extension
@@ -32,33 +86,9 @@ dc_graph.load_graph = function() {
     else if(/\.json$/.test(ignore_query(file1)))
         d3.json(file1, callback);
     else if(/\.gv|\.dot$/.test(ignore_query(file1)))
-        d3.text(file1, function (error, f) {
-            if(error) {
-                callback(error, null);
-                return;
-            }
-            var digraph = graphlibDot.parse(f);
-
-            var nodeNames = digraph.nodes();
-            var nodes = new Array(nodeNames.length);
-            nodeNames.forEach(function (name, i) {
-                var node = nodes[i] = digraph._nodes[nodeNames[i]];
-                node.id = i;
-                node.name = name;
-            });
-
-            var edgeNames = digraph.edges();
-            var edges = [];
-            edgeNames.forEach(function(e) {
-                var edge = digraph._edges[e];
-                edges.push({
-                    source: digraph._nodes[edge.u].id,
-                    target: digraph._nodes[edge.v].id,
-                    sourcename: edge.u,
-                    targetname: edge.v
-                });
-            });
-            var graph = {nodes: nodes, links: edges};
-            callback(null, graph);
-        });
+        d3.text(file1, process_dot.bind(null, callback));
+    else if(/\.psv$/.test(ignore_query(file1)))
+        d3.dsv('|', 'text/plain')(file1, process_dsv.bind(null, callback));
+    else if(/\.csv$/.test(ignore_query(file1)))
+        d3.csv(file1, process_dsv.bind(null, callback));
 };
