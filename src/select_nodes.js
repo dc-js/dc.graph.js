05993612ce10ccad8cbea0462b5e999e45e118c8
Gordon Woodhull <gordon@woodhull.com>
2017-07-17 15:03:38 -0400

    modes should not cause recursive drawing

--- src/select_nodes.js
+++ src/select_nodes.js
@@ -1,7 +1,7 @@
 dc_graph.select_nodes = function(props) {
     var select_nodes_group = dc_graph.select_nodes_group('select-nodes-group');
     var _selected = [], _oldSelected;
-    var _brush, _gBrush;
+    var _brush, _gBrush, _noRefresh;
 
     // http://stackoverflow.com/questions/7044944/jquery-javascript-to-detect-os-without-a-plugin
     var is_a_mac = navigator.platform.toUpperCase().indexOf('MAC')!==-1;
@@ -22,7 +22,8 @@ dc_graph.select_nodes = function(props) {
     function selection_changed(chart) {
         return function(selection) {
             _selected = selection;
-            chart.refresh();
+            if(!_noRefresh)
+                chart.refresh();
         };
     }
     function background_click_event(chart, v) {
@@ -105,8 +106,11 @@ dc_graph.select_nodes = function(props) {
             // drop any selected which no longer exist in the diagram
             var present = node.data().map(function(d) { return d.orig.key; });
             var now_selected = _selected.filter(function(k) { return present.indexOf(k) >= 0; });
-            if(_selected.length !== now_selected.length)
+            if(_selected.length !== now_selected.length) {
+                _noRefresh = true; // I hate these event processing flags, but we're in a draw and shouldn't trigger another
                 select_nodes_group.node_set_changed(now_selected);
+                _noRefresh = false;
+            }
         }
     }
 
