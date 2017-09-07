84c0dc346203589f540ce6b0f4b12caf062d813a
Gordon Woodhull <gordon@woodhull.com>
2017-09-07 08:14:37 -0400

    translate edittext code to d3

--- src/edit_text.js
+++ src/edit_text.js
@@ -2,52 +2,52 @@
 // http://stackoverflow.com/questions/9308938/inline-text-editing-in-svg/#26644652
 
 function edittext(svg, position, options) {
-    var foreign = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject');
-    var textdiv = document.createElement("div");
+    var foreign = svg.append('foreignObject').attr({
+        width: '100%',
+        height: '100%',
+        transform: 'translate(' + position.x + ' ' + position.y + ')'
+    });
+    var textdiv = foreign.append('xhtml:div');
     var text = options.text || "type on me";
-    var textnode = document.createTextNode(text);
-    textdiv.appendChild(textnode);
-    textdiv.setAttribute("contentEditable", "true");
-    textdiv.setAttribute("width", "auto");
-    textdiv.setAttribute("style", "display: inline-block; background-color: white; padding: 2px"); //to make div fit text
-    foreign.setAttribute("width", "100%");
-    foreign.setAttribute("height", "100%");
-    foreign.setAttributeNS(null, "transform", "translate(" + position.x + " " + position.y + ")");
-    svg.appendChild(foreign);
-    foreign.appendChild(textdiv);
+    textdiv.text(text).attr({
+        contenteditable: true,
+        width: 'auto'
+    }).style({
+        display: 'inline-block',
+        'background-color': 'white',
+        padding: '2px'
+    });
 
     function accept() {
-        options.accept && options.accept(textdiv.innerText);
-        textdiv.onblur = null;
+        options.accept && options.accept(textdiv.text());
+        textdiv.on('blur.edittext', null);
         foreign.remove();
     }
     function cancel() {
         options.cancel && options.cancel();
-        textdiv.onblur = null;
+        textdiv.on('blur.edittext', null);
         foreign.remove();
     }
 
-    textdiv.onkeydown = function(event) {
-        if(event.keyCode===13) {
-            event.preventDefault();
+    textdiv.on('keydown.edittext', function() {
+        if(d3.event.keyCode===13) {
+            d3.event.preventDefault();
         }
-    };
-    textdiv.onkeyup = function(event) {
-        if(event.keyCode===13) {
+    }).on('keyup.edittext', function() {
+        if(d3.event.keyCode===13) {
             accept();
-        } else if(event.keyCode===27) {
+        } else if(d3.event.keyCode===27) {
             cancel();
         }
-    };
-    textdiv.onblur = cancel;
-    textdiv.focus();
+    }).on('blur.edittext', cancel);
+    textdiv.node().focus();
 
     var range = document.createRange();
     if(options.selectText) {
-        range.selectNodeContents(textdiv);
+        range.selectNodeContents(textdiv.node());
     } else {
-        range.setStart(textdiv, text.length);
-        range.setEnd(textdiv, text.length);
+        range.setStart(textdiv.node(), text.length);
+        range.setEnd(textdiv.node(), text.length);
     }
     var sel = window.getSelection();
     sel.removeAllRanges();
@@ -56,6 +56,6 @@ function edittext(svg, position, options) {
 
 dc_graph.edit_text = function(svg, selection, options) {
     var position = options.position || {x: 0, y: 0};
-    edittext(svg.node(), position, options);
+    edittext(svg, position, options);
 };
 
