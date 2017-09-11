6402fda8a68347bba16f985a1f42e9bb1895b7f2
Gordon Woodhull <gordon@woodhull.com>
2017-09-11 15:33:09 -0400

    label_nodes -> label_things + customization

--- src/draw_graphs.js
+++ src/draw_graphs.js
@@ -1,6 +1,6 @@
 dc_graph.draw_graphs = function(options) {
     var select_nodes_group = dc_graph.select_things_group('select-nodes-group', 'select-nodes'),
-        label_nodes_group = dc_graph.label_nodes_group('label-nodes-group');
+        label_nodes_group = dc_graph.label_things_group('label-nodes-group', 'label-nodes');
     var _idTag = options.idTag || 'id',
         _edgeIdTag = options.edgeIdTag || _idTag,
         _sourceTag = options.sourceTag || 'source',
---
+++ src/label_things.js
@@ -0,0 +1,90 @@
+dc_graph.label_things = function(options) {
+    options = options || {};
+    var select_nodes_group = dc_graph.select_things_group(options.select_group, options.select_type),
+        label_nodes_group = dc_graph.label_things_group(options.label_type);
+    var _selected = [];
+    var _keyboard;
+
+    function selection_changed_listener(chart) {
+        return function(selection) {
+            _selected = selection;
+        };
+    }
+
+    function promise_identity(x) {
+        return Promise.resolve(x);
+    }
+    function grab_focus() {
+        _keyboard.focus();
+    }
+
+    function edit_label_listener(chart) {
+        return function(thing, eventOptions) {
+            var box = options.thing_box(thing);
+            dc_graph.edit_text(
+                chart.g(),
+                {
+                    text: eventOptions.text || options.thing_label(thing),
+                    align: options.align,
+                    box: box,
+                    selectText: eventOptions.selectText,
+                    accept: function(text) {
+                        return options.accept(thing, text)
+                            .then(grab_focus) // finallyc
+                            .catch(function(error) {
+                                grab_focus();
+                                throw error;
+                            });
+                    }
+                });
+        };
+    }
+
+    function add_behavior(chart, node, edge) {
+        _keyboard.on('keyup.' + options.label_type, function() {
+            if(_selected.length) {
+                // printable characters should start edit
+                if(d3.event.key.length !== 1)
+                    return;
+                var n2 = node.filter(function(d) {
+                    return chart.nodeKey.eval(d) === _selected[0];
+                });
+                if(n2.empty()) {
+                    console.error("couldn't find node '" + _selected[0] + "'!");
+                    return;
+                }
+                if(n2.size()>1) {
+                    console.error("found too many nodes for '" + _selected[0] + "' (" + n2.size() + ")!");
+                    return;
+                }
+                label_nodes_group.edit_label(n2, {text: d3.event.key, selectText: false});
+            }
+        });
+    }
+
+    function remove_behavior(chart, node, edge) {
+    }
+
+    var _behavior = dc_graph.behavior(options.label_type, {
+        add_behavior: add_behavior,
+        remove_behavior: remove_behavior,
+        parent: function(p) {
+            select_nodes_group.on('set_changed.' + options.label_type, p ? selection_changed_listener(p) : null);
+            label_nodes_group.on('edit_label.' + options.label_type, p ? edit_label_listener(p) : null);
+            if(p) {
+                _keyboard = p.child('keyboard');
+                if(!_keyboard)
+                    p.child('keyboard', _keyboard = dc_graph.keyboard());
+            }
+        }
+    });
+    return _behavior;
+};
+
+dc_graph.label_things_group = function(brushgroup, type) {
+    window.chart_registry.create_type(type, function() {
+        return d3.dispatch('edit_label');
+    });
+
+    return window.chart_registry.create_group(type, brushgroup);
+};
