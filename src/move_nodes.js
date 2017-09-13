63e3d98acef6b9f2f53b1d92a0872108069feb0d
Gordon Woodhull <gordon@woodhull.com>
2017-09-12 22:15:02 -0400

    first draft of move_nodes
    this works, but as expected it's a mess if you fix lots of nodes
    they should get unfixed somehow

--- src/move_nodes.js
+++ src/move_nodes.js
@@ -1,6 +1,7 @@
 dc_graph.move_nodes = function(sourceprops, moveprops) {
     var select_nodes_group = dc_graph.select_things_group('select-nodes-group', 'select-nodes');
-    var _selected = [], _moving;
+    var _selected = [], _startPos = null;
+    var _brush;
 
     // http://stackoverflow.com/questions/7044944/jquery-javascript-to-detect-os-without-a-plugin
     var is_a_mac = navigator.platform.toUpperCase().indexOf('MAC')!==-1;
@@ -21,19 +22,52 @@ dc_graph.move_nodes = function(sourceprops, moveprops) {
                 chart.refresh();
         };
     }
+    function for_each_selected(f) {
+        _selected.forEach(function(key) {
+            var n = _behavior.parent().getWholeNode(key);
+            f(n);
+        });
+    }
     function add_behavior(chart, node, edge) {
         chart.cascade(50, true, conditional_properties(function(n) {
-            return _moving && _selected.indexOf(n.orig.key) >= 0;
+            return _startPos && _selected.indexOf(n.orig.key) >= 0;
         }, null, sourceprops));
 
         node.on('mousedown.move-nodes', function(d) {
-            _moving = true;
+            _startPos = dc_graph.event_coords(chart);
+            for_each_selected(function(n) {
+                n.original_position = [n.cola.x, n.cola.y];
+            });
+            if(_brush)
+                _brush.deactivate();
         });
-        node.on('mousemove.move-nodes', function(d) {
-        });
-        node.on('mouseup.move-nodes', function(d) {
-            _moving = false;
-        });
+        function mouse_move() {
+            if(_startPos) {
+                var pos = dc_graph.event_coords(chart);
+                var dx = pos[0] - _startPos[0],
+                    dy = pos[1] - _startPos[1];
+                for_each_selected(function(n) {
+                    n.cola.x = n.original_position[0] + dx;
+                    n.cola.y = n.original_position[1] + dy;
+                });
+                chart.reposition(node, edge);
+            }
+        }
+        function mouse_up() {
+            if(_startPos) {
+                for_each_selected(function(n) {
+                    n.cola.dcg_nodeFixed = {x: n.cola.x, y: n.cola.y};
+                });
+                chart.redraw();
+                _startPos = null;
+            }
+        }
+        node
+            .on('mousemove.move-nodes', mouse_move)
+            .on('mouseup.move-nodes', mouse_up);
+        chart.svg()
+            .on('mousemove.move-nodes', mouse_move)
+            .on('mouseup.move-nodes', mouse_up);
     }
 
     function remove_behavior(chart, node, edge) {
@@ -47,7 +81,8 @@ dc_graph.move_nodes = function(sourceprops, moveprops) {
         add_behavior: add_behavior,
         remove_behavior: remove_behavior,
         parent: function(p) {
-            select_nodes_group.on('node_set_changed.move-nodes', p ? selection_changed(p) : null);
+            select_nodes_group.on('set_changed.move-nodes', p ? selection_changed(p) : null);
+            _brush = p.child('brush');
         }
     });
 
