e5e40680e4f36b59f199e0c03fa51e604ac446b2
Gordon Woodhull <gordon@woodhull.com>
2017-09-26 23:57:59 -0400

    allow delete_edges keyup to trigger b4 deleting neighboring edges
    it's the sharing of events thing - both delete_nodes and delete_edges are processing keyup of
    delete, so delete_nodes should let delete_edges delete any selected edges before making its own
    selection and deleting those. especially if those sets of edges overlap.

--- src/delete_nodes.js
+++ src/delete_nodes.js
@@ -10,13 +10,22 @@ dc_graph.delete_nodes = function(id_tag) {
         var deleteEdgesMode = diagram.child('delete-edges');
         if(!deleteEdgesMode)
             return null; // reject if we can't delete the edges
-        var deleteEdges = diagram.edgeGroup().all().filter(function(e) {
-            return nodes.indexOf(diagram.edgeSource()(e)) !== -1 ||
-                nodes.indexOf(diagram.edgeTarget()(e)) !== -1;
-        }).map(diagram.edgeKey());
-        select_edges_group.set_changed(deleteEdges);
-        return deleteEdgesMode.deleteSelection().then(function() {
-            return nodes;
+        // it is likely that the delete_edges mode is listening to the same keyup event we
+        // are. introduce a pause to let it process the delete key now, deleting any selected edges.
+        // then select any remaining edges connected to the selected nodes and delete those.
+        //
+        // more evidence that modes need to be able to say "i got this", or that we should have
+        // batch deletion. otoh, given the current behavior, delete_nodes deferring to delete_edges
+        // makes about as much sense as anything
+        return Promise.resolve(undefined).then(function() {
+            var deleteEdges = diagram.edgeGroup().all().filter(function(e) {
+                return nodes.indexOf(diagram.edgeSource()(e)) !== -1 ||
+                    nodes.indexOf(diagram.edgeTarget()(e)) !== -1;
+            }).map(diagram.edgeKey());
+            select_edges_group.set_changed(deleteEdges);
+            return deleteEdgesMode.deleteSelection().then(function() {
+                return nodes;
+            });
         });
     });
     return _behavior;
