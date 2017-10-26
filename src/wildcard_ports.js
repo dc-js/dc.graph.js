99307b7aea47024d33ebcfd49a6f4d2d91901e57
Gordon Woodhull <gordon@woodhull.com>
2017-10-26 03:51:20 -0400

    complete generic wildcard_ports

--- src/wildcard_ports.js
+++ src/wildcard_ports.js
@@ -1,7 +1,7 @@
 dc_graph.wildcard_ports = function(diagram, options) {
     var get_type = options.get_type || function(p) { return p.name; },
         set_type = options.set_type || function(p, type) { p.name = type; }, // harmful? feature may only work with type in data
-        get_wild = options.get_wild || function(p) { return p.orig.value.wild; },
+        is_wild = options.is_wild || function(p) { return p.orig.value.wild; },
         update_ports = options.update_ports || function() {};
     return {
         isValid: function(p1, p2) {
@@ -21,12 +21,14 @@ dc_graph.wildcard_ports = function(diagram, options) {
         resetTypes: function(edges)  {
             edges.forEach(function(eid) {
                 var e = diagram.getWholeEdge(eid);
-                var p = diagram.getPort(e.source.orig.key, null, e.orig.value.sourceport);
-                if(p.orig.value.wild && p.edges.length === 1)
-                    p.orig.value.type = null;
-                p = diagram.getPort(e.target.orig.key, null, e.orig.value.targetport);
-                if(p.orig.value.wild && p.edges.length === 1)
-                    p.orig.value.type = null;
+                var p = diagram.getPort(diagram.nodeKey.eval(e.source), null,
+                                        diagram.edgeSourcePortName.eval(e));
+                if(is_wild(p) && p.edges.length === 1)
+                    set_type(p, null);
+                var p = diagram.getPort(diagram.nodeKey.eval(e.target), null,
+                                        diagram.edgeTargetPortName.eval(e));
+                if(is_wild(p) && p.edges.length === 1)
+                    set_type(p, null);
             });
             return Promise.resolve(edges);
         }
