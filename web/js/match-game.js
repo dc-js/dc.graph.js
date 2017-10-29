35e428e9108d695f57f42e9ca0f8c9a4c62e7a24
Gordon Woodhull <gordon@woodhull.com>
2017-10-29 18:03:57 -0400

    match game use flexbox

--- web/js/match-game.js
+++ web/js/match-game.js
@@ -1,27 +1,62 @@
+var parentNodes = [
+    {
+        id: 'top',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 10
+    },
+    {
+        id: 'col-a',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        flex: 1
+    },
+    {
+        id: 'col-b',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        flex: 1
+    }
+];
+
 var data = d3.range(7).map(function(i) {
     return {
         id: 'a' + i,
         label: String.fromCharCode(97+i),
-        x: 100,
-        y: i*100+50
+        flex: 0
     };
 }).concat(d3.range(9).map(function(i) {
     return {
         id: 'b' + i,
         label: String.fromCharCode(48+i),
-        x: 200,
-        y: i*100+50
+        flex: 0
     };
 }));
 
-var node_flat = dc_graph.flat_group.make(data, n => n.id),
+var node_flat = dc_graph.flat_group.make(parentNodes.concat(data), n => n.id),
     edge_flat = dc_graph.flat_group.make([], e => e.id);
 
 var diagram = dc_graph.diagram('#graph')
-        .layoutEngine(dc_graph.manual_layout())
+        .layoutEngine(dc_graph.flexbox_layout()
+                      .addressToKey(function(ad) {
+                          switch(ad.length) {
+                          case 0: return 'top';
+                          case 1: return 'col-' + ad[0];
+                          case 2: return ad[0] + ad[1];
+                          default: throw new Error('not expecting more than depth 2');
+                          }
+                      })
+                      .keyToAddress(function(key) {
+                          if(key==='top') return [];
+                          else if(/^col-/.test(key)) return [key.split('col-')[1]];
+                          else if(/^(a|b)/.test(key)) return [key[0], +key.slice(1)];
+                          else throw new Error('couldn\'t parse key: ' + key);
+                      }))
         .width(1000).height(1000)
+        .mouseZoomable(false)
         .nodeDimension(node_flat.dimension).nodeGroup(node_flat.group)
-        .edgeDimension(edge_flat.dimension).edgeGroup(edge_flat.group);
+        .edgeDimension(edge_flat.dimension).edgeGroup(edge_flat.group)
+        .edgeLabel(null);
 
 var drawGraphs = dc_graph.draw_graphs({
     idTag: 'id',
