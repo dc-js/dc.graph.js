5da9cf4d26c4f0c939796f950ee5c1827bd8827e
Gordon Woodhull <gordon@woodhull.com>
2017-10-30 00:25:28 -0400

    randomize the number of category a and category b

--- web/js/match-game.js
+++ web/js/match-game.js
@@ -25,13 +25,13 @@ var parentNodes = [
     }
 ];
 
-var data = d3.range(7).map(function(i) {
+var data = d3.range(3 + Math.random()*7).map(function(i) {
     return {
         id: 'a' + i,
         label: String.fromCharCode(97+i),
         flex: 0
     };
-}).concat(d3.range(9).map(function(i) {
+}).concat(d3.range(3 + Math.random()*7).map(function(i) {
     return {
         id: 'b' + i,
         label: String.fromCharCode(48+i),
