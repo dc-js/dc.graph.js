080b8cc2132a1a8a36f941a09ccb47b9f86920a2
Gordon Woodhull <gordon@woodhull.com>
2017-11-01 14:46:23 -0400

    draw text and radius bounds
    i didn't know there was a "radius step" until that last fit_shape refactor
    not attempting to stylize all these, i figure you just see the steps as progressively larger bounds
    but choice/customization would be appreciated here

--- src/troubleshoot.js
+++ src/troubleshoot.js
@@ -24,35 +24,54 @@ dc_graph.troubleshoot = function() {
             opacity: _behavior.xhairOpacity() !== null ? _behavior.xhairOpacity() : _behavior.opacity(),
             stroke: _behavior.xhairColor()
         });
-        function boundary(point, wid, hei) {
-            return {
-                left: point.x - wid/2,
-                top: point.y - hei/2,
-                right: point.x + wid/2,
-                bottom: point.y + hei/2
-            };
-        };
         function cola_point(n) {
             return {x: n.cola.x, y: n.cola.y};
-        }
-        function corners(bounds) {
-            return [
-                bound_tick(bounds.left, bounds.top, _behavior.boundsWidth(), _behavior.boundsHeight()),
-                bound_tick(bounds.right, bounds.top, -_behavior.boundsWidth(), _behavior.boundsHeight()),
-                bound_tick(bounds.right, bounds.bottom, -_behavior.boundsWidth(), -_behavior.boundsHeight()),
-                bound_tick(bounds.left, bounds.bottom, _behavior.boundsWidth(), -_behavior.boundsHeight()),
-            ].join(' ');
-        }
-        function bound_tick(x, y, dx, dy) {
-            return 'M' + x + ',' + (y + dy) + ' v' + -dy + ' h' + dx;
         }
         var colabounds = node.data().map(function(n) {
             return boundary(cola_point(n), n.cola.width, n.cola.height);
         });
         var colaboundary = _debugLayer.selectAll('path.colaboundary').data(colabounds);
-        colaboundary.exit().remove();
-        colaboundary.enter().append('path').attr('class', 'colaboundary');
-        colaboundary.attr({
+        draw_corners(colaboundary, 'colaboundary');
+
+        var textbounds = node.data().map(function(n) {
+            if(!n.bbox)
+                return null;
+            return boundary(cola_point(n), n.bbox.width, n.bbox.height);
+        }).filter(function(n) { return !!n; });
+        var textboundary = _debugLayer.selectAll('path.textboundary').data(textbounds);
+        draw_corners(textboundary, 'textboundary');
+
+        var radiibounds = node.data().map(function(n) {
+            if(!typeof n.dcg_rx === 'number')
+                return null;
+            return boundary(cola_point(n), n.dcg_rx*2, n.dcg_ry*2);
+        }).filter(function(n) { return !!n; });
+        var radiiboundary = _debugLayer.selectAll('path.radiiboundary').data(radiibounds);
+        draw_corners(radiiboundary, 'radiiboundary');
+    }
+    function boundary(point, wid, hei) {
+        return {
+            left: point.x - wid/2,
+            top: point.y - hei/2,
+            right: point.x + wid/2,
+            bottom: point.y + hei/2
+        };
+    };
+    function bound_tick(x, y, dx, dy) {
+        return 'M' + x + ',' + (y + dy) + ' v' + -dy + ' h' + dx;
+    }
+    function corners(bounds) {
+        return [
+            bound_tick(bounds.left, bounds.top, _behavior.boundsWidth(), _behavior.boundsHeight()),
+            bound_tick(bounds.right, bounds.top, -_behavior.boundsWidth(), _behavior.boundsHeight()),
+            bound_tick(bounds.right, bounds.bottom, -_behavior.boundsWidth(), -_behavior.boundsHeight()),
+            bound_tick(bounds.left, bounds.bottom, _behavior.boundsWidth(), -_behavior.boundsHeight()),
+        ].join(' ');
+    }
+    function draw_corners(binding, classname) {
+        binding.exit().remove();
+        binding.enter().append('path').attr('class', classname);
+        binding.attr({
             d: corners,
             opacity: _behavior.boundsOpacity() !== null ? _behavior.boundsOpacity() : _behavior.opacity(),
             stroke: _behavior.boundsColor(),
