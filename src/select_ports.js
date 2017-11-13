af2b10f72b96363f6d3afc02d881d763cb2b6bbc
Gordon Woodhull <gordon@woodhull.com>
2017-11-13 14:33:00 -0500

    allow specifying which style of ports should be highlighted
    on selection

--- src/select_ports.js
+++ src/select_ports.js
@@ -1,5 +1,6 @@
 dc_graph.select_ports = function(props, options) {
     options = options || {};
+    var port_style = options.portStyle || 'symbols';
     var select_ports_group = dc_graph.select_things_group(options.select_ports_group || 'select-ports-group', 'select-ports');
     var thinginess = {
         laterDraw: true,
@@ -14,10 +15,10 @@ dc_graph.select_ports = function(props, options) {
             {edge: _behavior.parent().edgeKey.eval(p.edges[0]), name: p.name};
         },
         applyStyles: function(pred) {
-            _behavior.parent().portStyle('symbols').cascade(50, true, conditional_properties(pred, props));
+            _behavior.parent().portStyle(port_style).cascade(50, true, conditional_properties(pred, props));
         },
         removeStyles: function() {
-            _behavior.parent().portStyle('symbols').cascade(50, false, props);
+            _behavior.parent().portStyle(port_style).cascade(50, false, props);
         },
         keysEqual: function(k1, k2) {
             return k1.name === k2.name && (k1.node ? k1.node === k2.node : k1.edge === k2.edge);
