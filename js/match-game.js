9ca928d8e4f847ffc49783cd84c4b6dea7dfc8d7
Gordon Woodhull <gordon@mt-asa1-vpn249.research.att.com>
2017-12-07 23:06:04 -0500

    Synced from from master branch.

---
+++ js/match-game.js
@@ -0,0 +1,194 @@
+var qs = querystring.parse();
+var options = Object.assign({
+    min: 3,
+    max: 10
+}, qs);
+
+var vertical = options.rankdir === 'TB';
+// intentionally using a dumb identifier scheme: 'top, 'col-a', .. 'a1 .. 'b3' for generality's sake
+// it would be smarter irl to use key = prefix + addr.join(delimiter)
+var parentNodes = [
+    {
+        id: 'top',
+        flexDirection: vertical ? 'column' : 'row',
+        justifyContent: 'space-between',
+        padding: 10
+    },
+    {
+        id: 'col-a',
+        flexDirection: vertical ? 'row' : 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-end',
+        flex: 0
+    },
+    {
+        id: 'col-b',
+        flexDirection: vertical ? 'row' : 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flex: 0
+    }
+];
+
+var adjectives = ['strong', 'happy', 'libelious', 'rainy', 'effulgent', 'bookish'],
+    nouns = ['steel', 'book', 'cat', 'talk', 'road', 'umbrella', 'nimwit'];
+
+function rndsel(a) {
+    return a[Math.floor(Math.random()*a.length)];
+}
+
+function phrase() {
+    return rndsel(adjectives) + ' ' + rndsel(nouns);
+}
+
+var data = d3.range(Math.round(+options.min + Math.random()*(options.max-options.min))).map(function(i) {
+    return {
+        id: 'a' + i,
+        label: [String.fromCharCode(97+i) + '.', phrase()],
+        flex: 0
+    };
+}).concat(d3.range(Math.round(+options.min + Math.random()*(options.max-options.min))).map(function(i) {
+    return {
+        id: 'b' + i,
+        label: [String.fromCharCode(48+i) + '.', phrase()],
+        flex: 0
+    };
+}));
+
+var lbounds = [Math.PI-1, Math.PI+1], rbounds = [-1,1], ubounds = [-Math.PI/2-1, -Math.PI/2+1], dbounds = [Math.PI/2-1, Math.PI/2+1];
+var inbounds, outbounds;
+if(vertical) {
+    inbounds = ubounds;
+    outbounds = dbounds;
+} else  {
+    inbounds = lbounds;
+    outbounds = rbounds;
+}
+var ports = data.map(n => ({
+    nodeId: n.id,
+    side: n.id[0] === 'a' ? 'out' : 'in',
+    bounds: n.id[0] === 'a' ? outbounds : inbounds
+}));
+
+var node_flat = dc_graph.flat_group.make(parentNodes.concat(data), n => n.id),
+    edge_flat = dc_graph.flat_group.make([], e => e.id),
+    port_flat = dc_graph.flat_group.make(ports, p => p.nodeId + '/' + p.side);
+
+var layout = dc_graph.flexbox_layout()
+    .logStuff(true)
+    .addressToKey(function(ad) {
+        switch(ad.length) {
+        case 0: return 'top';
+        case 1: return 'col-' + ad[0];
+        case 2: return ad[0] + ad[1];
+        default: throw new Error('not expecting more than depth 2');
+        }
+    })
+    .keyToAddress(function(key) {
+        if(key==='top') return [];
+        else if(/^col-/.test(key)) return [key.split('col-')[1]];
+        else if(/^(a|b)/.test(key)) return [key[0], +key.slice(1)];
+        else throw new Error('couldn\'t parse key: ' + key);
+    });
+
+var diagram = dc_graph.diagram('#graph')
+        .layoutEngine(layout)
+        .width(null).height(null)
+        .transitionDuration(250)
+        .layoutUnchanged(true)
+        .mouseZoomable(false)
+        .nodeDimension(node_flat.dimension).nodeGroup(node_flat.group)
+        .edgeDimension(edge_flat.dimension).edgeGroup(edge_flat.group)
+        .portDimension(port_flat.dimension).portGroup(port_flat.group)
+        .nodeShape(n => layout.keyToAddress()(diagram.nodeKey()(n)).length < 2 ? 'nothing' : 'rounded-rect')
+        .nodeLabelPadding({x: 20, y: 0})
+        .nodeLabelAlignment(n => /^a/.test(n.key) ? 'right' : 'left')
+        .nodeStrokeWidth(0)
+        .nodeTitle(null)
+        .edgesInFront(true)
+        .edgeSourcePortName('out')
+        .edgeTargetPortName('in')
+        .edgeLabel(null)
+        .portNodeKey(p => p.value.nodeId)
+        .portName(p => p.value.side)
+        .portBounds(p => p.value.bounds)
+        .portElastic(false);
+
+diagram.child('validate', dc_graph.validate());
+if(options.trouble)
+    diagram.child('troubleshoot', dc_graph.troubleshoot());
+diagram.child('place-ports', dc_graph.place_ports());
+
+var circlePorts = dc_graph.symbol_port_style()
+        .portSymbol(null)
+        .displacement(0)
+        .smallRadius(2).mediumRadius(4).largeRadius(6)
+        .outlineStroke('black').outlineStrokeWidth(1);
+diagram.portStyle('circle-ports', circlePorts)
+    .portStyleName('circle-ports');
+
+var drawGraphs = dc_graph.draw_graphs({
+    idTag: 'id',
+    sourceTag: 'sourcename',
+    targetTag: 'targetname'
+})
+        .usePorts(true)
+        .clickCreatesNodes(false)
+        .edgeCrossfilter(edge_flat.crossfilter);
+
+diagram.child('draw-graphs', drawGraphs);
+
+var select_edges = dc_graph.select_edges({
+    edgeStroke: 'lightblue',
+    edgeStrokeWidth: 3
+}).multipleSelect(false);
+diagram.child('select-edges', select_edges);
+
+var select_edges_group = dc_graph.select_things_group('select-edges-group', 'select-edges');
+var delete_edges = dc_graph.delete_things(select_edges_group, 'delete-edges', 'id')
+        .crossfilterAccessor(function(chart) {
+            return edge_flat.crossfilter;
+        })
+        .dimensionAccessor(function(chart) {
+            return diagram.edgeDimension();
+        });
+diagram.child('delete-edges', delete_edges);
+
+var oppositeMatcher = dc_graph.match_opposites(diagram, {
+    edgeStroke: 'orangered'
+}, {
+    delete_edges: delete_edges
+});
+drawGraphs.conduct(oppositeMatcher);
+
+if(qs.selports) {
+    var select_ports = dc_graph.select_ports({
+        portBackgroundFill: 'lightgreen',
+        outlineStroke: 'orange',
+        outlineStrokeWidth: 2,
+        smallRadius: 5,
+        mediumRadius: 7,
+        largeRadius: 10
+    }, {
+        portStyle: 'circle-ports'
+    }).multipleSelect(false);
+    diagram.child('select-ports', select_ports);
+    var select_ports_group = dc_graph.select_things_group('select-ports-group', 'select-ports');
+    select_ports_group.on('set_changed.show-info', function(ports) {
+        if(ports.length>0) {
+            select_edges_group.set_changed([]);
+        }
+    });
+}
+
+
+dc.renderAll();
+
+$('#resize').resizable({
+    resize: function(event, ui) {
+        diagram
+            .width(null)
+            .height(null)
+            .redraw();
+    }
+});
--- js/network-building.js
+++ js/network-building.js
@@ -4,7 +4,7 @@ var options = Object.assign({
 }, qs);
 
 var node_flat = dc_graph.flat_group.make([], function(d) { return d.id; }),
-    edge_flat = dc_graph.flat_group.make([], function(d) { return d.source + '-' + d.target; });
+    edge_flat = dc_graph.flat_group.make([], function(d) { return d.id; });
 
 var diagram = dc_graph.diagram('#graph');
 var engine = dc_graph.spawn_engine(options.layout, options, options.worker != 'false');
@@ -21,7 +21,10 @@ diagram
     .edgeDimension(edge_flat.dimension).edgeGroup(edge_flat.group)
     .edgeSource(function(e) { return e.value.source; })
     .edgeTarget(function(e) { return e.value.target; })
+    .nodeShape(qs.shape || 'ellipse')
     .nodeLabel(function(n) { return n.value.label; })
+    .edgeLabel(function(e) { return e.value.label || ''; })
+    .nodeLabelPadding({x: 4, y: 4})
     .nodeFixed(function(n) { return n.value.fixedPos; })
     .edgeArrowhead('vee');
 
@@ -32,13 +35,36 @@ var select_nodes = dc_graph.select_nodes({
     nodeStrokeWidth: 2
 }).multipleSelect(false);
 
-var label_nodes = dc_graph.label_nodes({
-    nodeCrossfilter: node_flat.crossfilter
-});
+var select_edges = dc_graph.select_edges({
+    edgeStroke: 'darkgreen',
+    edgeStrokeWidth: 2
+}).multipleSelect(false);
+
+var label_nodes = dc_graph.label_nodes(),
+    label_edges = dc_graph.label_edges();
+
+var delete_nodes = dc_graph.delete_nodes()
+        .crossfilterAccessor(function(chart) {
+            return node_flat.crossfilter;
+        })
+        .dimensionAccessor(function(chart) {
+            return node_flat.dimension;
+        });
+
+var delete_edges = dc_graph.delete_things(
+    dc_graph.select_things_group('select-edges-group', 'select-edges'),
+    'delete-edges')
+        .crossfilterAccessor(function(chart) {
+            return edge_flat.crossfilter;
+        })
+        .dimensionAccessor(function(chart) {
+            return edge_flat.dimension;
+        });
 
 var timestamp = 0;
 function add_object(d) {
     d.timestamp = timestamp++;
+    return Promise.resolve(d);
 }
 
 var draw_graphs = dc_graph.draw_graphs({
@@ -48,8 +74,24 @@ var draw_graphs = dc_graph.draw_graphs({
 
 diagram
     .child('select-nodes', select_nodes)
+    .child('select-edges', select_edges)
     .child('label-nodes', label_nodes)
-    .child('draw-graphs', draw_graphs);
+    .child('label-edges', label_edges)
+    .child('draw-graphs', draw_graphs)
+    .child('delete-nodes', delete_nodes)
+    .child('delete-edges', delete_edges);
+
+// make node selection and edge selection mutually exclusive
+var select_nodes_group = dc_graph.select_things_group('select-nodes-group', 'select-nodes');
+var select_edges_group = dc_graph.select_things_group('select-edges-group', 'select-edges');
+select_nodes_group.on('set_changed.show-info', function(nodes) {
+    if(nodes.length)
+        select_edges_group.set_changed([]); // selecting node clears selected edge
+});
+select_edges_group.on('set_changed.show-info', function(edges) {
+    if(edges.length)
+        select_nodes_group.set_changed([]); // selecting edge clears selected node
+});
 
 var nodeDim = node_flat.crossfilter.dimension(function(d) { return d.timestamp; });
 var outnodes = dc.dataTable('#output-nodes')
@@ -91,6 +133,12 @@ var outedges = dc.dataTable('#output-edges')
                 label: 'Target',
                 format: function(d) {
                     return node_labels[d.target];
+                }
+            },
+            {
+                label: 'Label',
+                format: function(d) {
+                    return d.label;
                 }
             }
         ]);
