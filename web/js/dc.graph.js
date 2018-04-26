e6a15aa9df83c2ebc8e806399ed48f5aec80095e
Gordon Woodhull <gordon@woodhull.com>
2018-04-26 16:58:24 -0400

    artifacts and dependencies

--- web/js/dc.graph.js
+++ web/js/dc.graph.js
@@ -1,5 +1,5 @@
 /*!
- *  dc.graph 0.6.0-beta.3
+ *  dc.graph 0.6.0-beta.4
  *  http://dc-js.github.io/dc.graph.js/
  *  Copyright 2015-2016 AT&T Intellectual Property & the dc.graph.js Developers
  *  https://github.com/dc-js/dc.graph.js/blob/master/AUTHORS
@@ -28,7 +28,7 @@
  * instance whenever it is appropriate.  The getter forms of functions do not participate in function
  * chaining because they return values that are not the diagram.
  * @namespace dc_graph
- * @version 0.6.0-beta.3
+ * @version 0.6.0-beta.4
  * @example
  * // Example chaining
  * diagram.width(600)
@@ -38,7 +38,7 @@
  */
 
 var dc_graph = {
-    version: '0.6.0-beta.3',
+    version: '0.6.0-beta.4',
     constants: {
         CHART_CLASS: 'dc-graph'
     }
@@ -4322,7 +4322,7 @@ dc_graph.engines = {
         if(entry.name)
             this.unregister(entry.name);
         else if(entry.names)
-            this.names.forEach(function(layoutName) {
+            entry.names.forEach(function(layoutName) {
                 that.unregister(layoutName);
             });
         else {
