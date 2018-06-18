7fa713d4a2d22a66448e497b025a787f3d0c6247
Gordon Woodhull <gordon@woodhull.com>
2018-06-18 14:25:04 -0400

    highlight_neighbors both include & exclude both nodes & edges

--- src/highlight_neighbors.js
+++ src/highlight_neighbors.js
@@ -1,19 +1,36 @@
-dc_graph.highlight_neighbors = function(props) {
+dc_graph.highlight_neighbors = function(includeprops, excludeprops) {
+    var _hovered = false;
     function clear_all_highlights(edge) {
         edge.each(function(e) {
             e.dcg_highlighted = false;
         });
+        _hovered = false;
     }
 
     function add_behavior(diagram, node, edge) {
-        diagram.cascade(100, true, node_edge_conditions(null, function(e) {
-            return e.dcg_highlighted;
-        }, props));
+        diagram.cascade(100, true, node_edge_conditions(
+            function(n) {
+                return n.dcg_highlighted;
+            }, function(e) {
+                return e.dcg_highlighted;
+            }, includeprops));
+        diagram.cascade(110, true, node_edge_conditions(
+            function(n) {
+                return _hovered && !n.dcg_highlighted;
+            }, function(e) {
+                return _hovered && !e.dcg_highlighted;
+            }, excludeprops));
         node
             .on('mouseover.highlight-neighbors', function(n) {
+                node.each(function(n) {
+                    n.dcg_highlighted = false;
+                });
                 edge.each(function(e) {
                     e.dcg_highlighted = e.source === n || e.target === n;
+                    if(e.dcg_highlighted)
+                        e.source.dcg_highlighted = e.target.dcg_highlighted = true;
                 });
+                _hovered = true;
                 diagram.refresh(node, edge);
             })
             .on('mouseout.highlight-neighbors', function(n) {
@@ -27,7 +44,7 @@ dc_graph.highlight_neighbors = function(props) {
             .on('mouseover.highlight-neighbors', null)
             .on('mouseout.highlight-neighbors', null);
         clear_all_highlights(edge);
-        diagram.cascade(100, false, props);
+        diagram.cascade(100, false, includeprops);
     }
 
     return dc_graph.behavior('highlight-neighbors', {
