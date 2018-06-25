dc-js/dc.graph.js

commit 08fb9ba01147b84cd45bb1824f4ca4ecb032d274

nodeLabelPadding can be either number or {x,y} object, document it.

src/node_contents.js
@@ -75,7 +75,7 @@ dc_graph.with_icon_contents = function(contents, width, height) {
             contents.parent(parent);
         }),
         padding: function(n) {
-            var padding = _contents.parent().nodeLabelPadding.eval(n);
+            var padding = node_label_padding(_contents.parent(), n);
             return {
                 x: padding.x * 3,
                 y: padding.y * 3
@@ -99,7 +99,7 @@ dc_graph.with_icon_contents = function(contents, width, height) {
                 href: _contents.parent().nodeIcon.eval,
                 x: function(n) {
                     var totwid = width + contents.textbox(d3.select(this.parentNode)).width;
-                    return -totwid/2 - _contents.parent().nodeLabelPadding.eval(n).x;
+                    return -totwid/2 - node_label_padding(_contents.parent(), n);
                 },
                 y: -height/2
             });

src/diagram.js
@@ -468,7 +468,18 @@ dc_graph.diagram = function (parent, chartGroup) {
      **/
     _diagram.nodePadding = property(6);
 
-    _diagram.nodeLabelPadding = property({x: 0, y: 0});
+
+    /**
+     * Set or get the padding, in pixels, for a node's label. If an object, should contain fields
+     * `x` and `y`. If a number, will be applied to both x and y.
+     * @method nodeLabelPadding
+     * @memberof dc_graph.diagram
+     * @instance
+     * @param {Function|Number|Object} [nodeLabelPadding=0]
+     * @return {Function|Number}
+     * @return {dc_graph.diagram}
+     **/
+    _diagram.nodeLabelPadding = property(0);
 
     /**
      * Set or get the line height for nodes with multiple lines of text, in ems.

src/shape.js
@@ -203,6 +203,13 @@ function shape_changed(diagram) {
     };
 }
 
+function node_label_padding(diagram, n) {
+    var nlp = diagram.nodeLabelPadding.eval(n);
+    if(typeof nlp === 'number' || typeof nlp === 'string')
+        return {x: +nlp, y: +nlp};
+    else return nlp;
+}
+
 function fit_shape(shape, diagram) {
     return function(content) {
         content.each(function(n) {
@@ -215,7 +222,7 @@ function fit_shape(shape, diagram) {
                 if(content && diagram.content(content).padding)
                     padding = diagram.content(content).padding(n);
                 else {
-                    var padding2 = diagram.nodeLabelPadding.eval(n);
+                    var padding2 = node_label_padding(diagram, n);
                     padding = {
                         x: padding2.x*2,
                         y: padding2.y*2

commit 6f1452e124a4116035e8c1d80801cabe08366cc7

fix icon positioning
broken in 08fb9ba01147b8

src/node_contents.js
@@ -99,7 +99,7 @@ dc_graph.with_icon_contents = function(contents, width, height) {
                 href: _contents.parent().nodeIcon.eval,
                 x: function(n) {
                     var totwid = width + contents.textbox(d3.select(this.parentNode)).width;
-                    return -totwid/2 - node_label_padding(_contents.parent(), n);
+                    return -totwid/2 - node_label_padding(_contents.parent(), n).x;
                 },
                 y: -height/2
             });