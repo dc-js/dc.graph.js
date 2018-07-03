827aae582b5436f6fd6a73c48e803ffdf0abf568
Gordon Woodhull <gordon@woodhull.com>
2018-07-03 17:42:05 -0400

    display caret only when legend node is hovered
    add some space so that there is no gap between text and caret

--- src/legend.js
+++ src/legend.js
@@ -132,16 +132,26 @@ dc_graph.legend = function() {
                 .attr('font-size', '75%')
                 .attr('fill', 'blue')
                 .attr('class', 'dropdown-caret')
-                .html('&#x25BC;');
+                .style('visibility', 'hidden')
+                .html('&emsp;&#x25BC;');
             caret
                 .attr('dx', function(d) {
-                    return (_legend.nodeWidth()/2+_legend.gap()) + getBBoxNoThrow(d3.select(this.parentNode).select('text.legend-label').node()).width + 5;
+                    return (_legend.nodeWidth()/2+_legend.gap()) + getBBoxNoThrow(d3.select(this.parentNode).select('text.legend-label').node()).width;
                 })
                 .on('mouseenter', function(d) {
                     var rect = this.getBoundingClientRect();
                     var key = _legend.parent().nodeKey.eval(d);
                     _legend.dropdown()
                         .show(key, rect.x, rect.y);
+                });
+            node
+                .on('mouseenter', function(d) {
+                    d3.select(this).selectAll('.dropdown-caret')
+                        .style('visibility', 'visible');
+                })
+                .on('mouseleave', function(d) {
+                    d3.select(this).selectAll('.dropdown-caret')
+                        .style('visibility', 'hidden');
                 });
         }
 
