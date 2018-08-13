51453a52aa990bda5692fab4dbf0a153d3cd2f61
Gordon Woodhull <gordon@woodhull.com>
2018-08-13 08:32:02 -0400

    a simple tip.disabled()
    it's ugly that this still listens for events and then ignores them
    but that's part of the endemic problem where modes
    should be enabled/disabled automatically based on priority
    or "action sequence matching",
    and the specific problem of dissonance between only knowing graph objects on event firing
    but also wanting modes to be stateful "object-oriented" objects

--- src/tip.js
+++ src/tip.js
@@ -14,7 +14,7 @@
 dc_graph.tip = function(options) {
     options = options || {};
     var _namespace = options.namespace || 'tip';
-    var _d3tip = null;
+    var _d3tip = null, _disabled;
     var _showTimeout, _hideTimeout;
     var _dispatch = d3.dispatch('tipped');
 
@@ -30,6 +30,10 @@ dc_graph.tip = function(options) {
         }
     }
     function fetch_and_show_content(d) {
+        if(_disabled) {
+            hide_tip.call(this);
+            return;
+        }
         var target = this,
             next = function() {
                 _behavior.content()(d, function(content) {
@@ -142,6 +146,13 @@ dc_graph.tip = function(options) {
         return _dispatch.on(event, f);
     };
 
+    _behavior.disabled = function(whether) {
+        if(!arguments.length)
+            return _disabled;
+        _disabled = whether;
+        return _behavior;
+    };
+
     _behavior.displayTip = function(filter, n, cb) {
         if(typeof filter !== 'function') {
             var d = filter;
@@ -163,6 +174,7 @@ dc_graph.tip = function(options) {
                 cb(d);
         }
     };
+
     _behavior.hideTip = function() {
         if(_d3tip)
             hide_tip();
