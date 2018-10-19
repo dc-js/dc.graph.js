71c279bc3cf73bccf97c87e6ca8ac205de223ecd
Gordon Woodhull <gordon@woodhull.com>
2018-10-19 09:34:12 -0400

    get arrowhead/tail orientation much closer
    bisect to less than 25 pixels rather than strict t=0.25 or 0.75

--- src/diagram.js
+++ src/diagram.js
@@ -2031,10 +2031,14 @@ dc_graph.diagram = function (parent, chartGroup) {
 
     function calculate_arrowhead_orientation(points, end) {
         var spos = points[0], tpos = points[points.length-1];
-        var partial = bezier_point(points, end === 'tail' ? 0.25 : 0.75);
-        return (end === 'head' ?
-                Math.atan2(tpos.y - partial.y, tpos.x - partial.x) :
-                Math.atan2(spos.y - partial.y, spos.x - partial.x)) + 'rad';
+        var ref = end === 'head' ? tpos : spos;
+        var partial, t = 0.5;
+        do {
+            t = (end === 'head' ? 1 + t : t) / 2;
+            partial = bezier_point(points, t);
+        }
+        while(Math.hypot(ref.x - partial.x, ref.y - partial.y) > 25);
+        return Math.atan2(ref.y - partial.y, ref.x - partial.x) + 'rad';
     }
 
     function enforce_path_direction(path, spos, tpos) {
