05690a998b3368d0d80af7f2e79fe9bd3cbf30ca
Gordon Woodhull <gordon@woodhull.com>
2018-10-29 17:13:29 -0400

    infinite grid

--- src/grid.js
+++ src/grid.js
@@ -11,28 +11,28 @@ dc_graph.grid = function() {
             _gridLayer.remove();
     }
 
-    function on_zoom(translate, scale) {
+    function on_zoom(translate, scale, xDomain, yDomain) {
         if(_gridLayer) {
-            var vline_data = scale >= _behavior.threshold() ? d3.range(100) : [];
+            var vline_data = scale >= _behavior.threshold() ? d3.range(Math.floor(xDomain[0]), Math.ceil(xDomain[1]) + 1) : [];
             var vlines = _gridLayer.selectAll('line.grid-line.vertical').data(vline_data);
             vlines.exit().remove();
             vlines.enter().append('line')
                 .attr('class', 'grid-line vertical');
             vlines.attr({
                 x1: function(d) { return d - 0.5; },
-                y1: 0,
+                y1: yDomain[0],
                 x2: function(d) { return d - 0.5; },
-                y2: 100
+                y2: yDomain[1]
             });
-            var hline_data = scale >= _behavior.threshold() ? d3.range(100) : [];
+            var hline_data = scale >= _behavior.threshold() ? d3.range(Math.floor(yDomain[0]), Math.ceil(yDomain[1]) + 1) : [];
             var hlines = _gridLayer.selectAll('line.grid-line.horizontal').data(hline_data);
             hlines.exit().remove();
             hlines.enter().append('line')
                 .attr('class', 'grid-line horizontal');
             hlines.attr({
-                x1: 0,
+                x1: xDomain[0],
                 y1: function(d) { return d - 0.5; },
-                x2: 100,
+                x2: xDomain[1],
                 y2: function(d) { return d - 0.5; }
             });
         }
