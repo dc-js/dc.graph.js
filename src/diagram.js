e1f652863825b55109bdc8867b5b59822356a555
Gordon Woodhull <gordon@woodhull.com>
2018-12-06 16:24:03 -0500

    ports contribute to bounds

--- src/diagram.js
+++ src/diagram.js
@@ -2136,8 +2136,15 @@ dc_graph.diagram = function (parent, chartGroup) {
     }
 
     function node_bounds(n) {
-        return {left: n.cola.x - n.dcg_rx, top: n.cola.y - n.dcg_ry,
-                right: n.cola.x + n.dcg_rx, bottom: n.cola.y + n.dcg_ry};
+        var bounds = {left: n.cola.x - n.dcg_rx, top: n.cola.y - n.dcg_ry,
+                      right: n.cola.x + n.dcg_rx, bottom: n.cola.y + n.dcg_ry};
+        _nodePorts[_diagram.nodeKey.eval(n)].forEach(function(p) {
+            var pb = _diagram.portStyle(_diagram.portStyleName.eval(p)).portBounds(p);
+            pb.left += n.cola.x; pb.top += n.cola.y;
+            pb.right += n.cola.x; pb.bottom += n.cola.y;
+            bounds = union_bounds(bounds, pb);
+        });
+        return bounds;
     }
 
     function union_bounds(b1, b2) {
