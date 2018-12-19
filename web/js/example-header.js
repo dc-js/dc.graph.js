f6cfea0899240646d21ca495f21bb2c1fa670dbf
Gordon Woodhull <gordon@woodhull.com>
2018-12-19 10:26:46 -0500

    include title in header

--- web/js/example-header.js
+++ web/js/example-header.js
@@ -5,9 +5,9 @@
     document.write([
         '<div id="header" style="padding: 1em;">',
         '<a href="."><span style="font-size: 24px; font-weight: bold;">dc.graph</span></a>',
-        '&emsp;&emsp;&emsp;<span id="links">',
-        '<a href="',
-        'https://github.com/dc-js/dc.graph.js/tree/develop/web/js/' + jsFilename,
+        '<span style="font-size: 18px; padding-left: 5em;">', document.title, '</span>',
+        '<span id="links" style="padding-left: 3em; font-size: 12px;">',
+        '<a href="https://github.com/dc-js/dc.graph.js/tree/develop/web/js/' + jsFilename,
         '">source</a>',
         '<span id="version" style="position: absolute; right: 2em; top: 2em"></span>',
         '</span>',
