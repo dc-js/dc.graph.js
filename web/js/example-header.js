35bee933289a65f8a4c6a59214bf9e26a89ee491
Gordon Woodhull <gordon@woodhull.com>
2018-12-24 10:07:26 -0500

    right align the source link

--- web/js/example-header.js
+++ web/js/example-header.js
@@ -6,10 +6,10 @@
         '<div id="header" style="padding: 1em;">',
         '<a href="."><span style="font-size: 24px; font-weight: bold;">dc.graph</span></a>',
         '<span style="font-size: 18px; padding-left: 5em;">', document.title, '</span>',
-        '<span id="links" style="padding-left: 3em; font-size: 12px;">',
+        '<span id="right-header" style="position: absolute; right: 2em; top: 2em; font-size: 12px;">',
         '<a href="https://github.com/dc-js/dc.graph.js/tree/develop/web/js/' + jsFilename,
         '">source</a>',
-        '<span id="version" style="position: absolute; right: 2em; top: 2em"></span>',
+        '<span id="version" style="padding-left: 2em;"></span>',
         '</span>',
         '</div>'
     ].join(''));
