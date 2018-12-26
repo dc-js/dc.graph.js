b5b9c889e8d6e1e2841af957f23044b9a24dadf0
Gordon Woodhull <gordon@woodhull.com>
2018-12-26 15:50:36 -0500

    document brush

--- src/brush.js
+++ src/brush.js
@@ -1,3 +1,10 @@
+/**
+ * `dc_graph.brush` is a {@link dc_graph.mode mode} providing a simple wrapper over
+ * [d3.svg.brush](https://github.com/d3/d3-3.x-api-reference/blob/master/SVG-Controls.md#brush)
+ * @class brush
+ * @memberof dc_graph
+ * @return {dc_graph.brush}
+ **/
 dc_graph.brush = function() {
     var _brush = null, _gBrush, _dispatch = d3.dispatch('brushstart', 'brushmove', 'brushend');
 
@@ -37,20 +44,51 @@ dc_graph.brush = function() {
         remove: remove_brush
     });
 
+    /**
+     * Subscribe to a brush event, currently `brushstart`, `brushmove`, or `brushend`
+     * @method on
+     * @memberof dc_graph.brush
+     * @instance
+     * @param {String} event the name of the event; please namespace with `'namespace.event'`
+     * @param {Function} [f] the handler function; if omitted, returns the current handler
+     * @return {dc_graph.brush}
+     * @return {Function}
+     **/
     _mode.on = function(event, f) {
         if(arguments.length === 1)
             return _dispatch.on(event);
         _dispatch.on(event, f);
         return this;
     };
+    /**
+     * Add the brush to the parent diagram's SVG
+     * @method activate
+     * @memberof dc_graph.brush
+     * @instance
+     * @return {dc_graph.brush}
+     **/
     _mode.activate = function() {
         install_brush(_mode.parent());
         return this;
     };
+    /**
+     * Remove the brush from the parent diagram's SVG
+     * @method deactivate
+     * @memberof dc_graph.brush
+     * @instance
+     * @return {dc_graph.brush}
+     **/
     _mode.deactivate = function() {
         remove_brush();
         return this;
     };
+    /**
+     * Retrieve whether the brush is currently active
+     * @method isActive
+     * @memberof dc_graph.brush
+     * @instance
+     * @return {Boolean}
+     **/
     _mode.isActive = function () {
         return !!_gBrush;
     };
