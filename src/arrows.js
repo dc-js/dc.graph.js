abe07c10b9b190e0110b19e2eb36099a983cb269
Gordon Woodhull <gordon@woodhull.com>
2018-12-27 15:04:14 -0500

    cut edge splines and draw arrows on another copy of path
    the arrows go on a copy of the original edge spline
    invisible because no stroke defined
    
    this is a squash of many commits; those messages preserved below
    
    1. attempt: chop splines at arrowheads instead of dashing
    
    however, now the markers are shown on the chopped spline :-(
    
    2. separate the paths that support arrows from the paths that are drawn
    
    further attempts to chop splines
    i live in hell
    
    3. flesh out a real binary search
    
    seems maybe efficient enough, but this is scary
    
    4. tighten up binary search
    
    this is where i stop seeing it help on some translucent arrows (enron demo)
    there are apparently other bugs
    
    5. dt cutoff should be last resort, exactitude EPS in "pixels"
    
    6. consider source and target node stroke width when chopping splines
    
    why doesn't draw_edge_to_shapes take care of all of this?
    
    7. remove dash_edges_for_arrows
    
    8. troubleshoot use full edge spline, not chopped one

--- src/arrows.js
+++ src/arrows.js
@@ -1,15 +1,3 @@
-function calculate_arrowhead_orientation(points, end) {
-    var spos = points[0], tpos = points[points.length-1];
-    var ref = end === 'head' ? tpos : spos;
-    var partial, t = 0.5;
-    do {
-        t = (end === 'head' ? 1 + t : t) / 2;
-        partial = bezier_point(points, t);
-    }
-    while(Math.hypot(ref.x - partial.x, ref.y - partial.y) > 25);
-    return Math.atan2(ref.y - partial.y, ref.x - partial.x) + 'rad';
-}
-
 function offsetx(ofsx) {
     return function(p) {
         return {x: p.x + ofsx, y: p.y};
