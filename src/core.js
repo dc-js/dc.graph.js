2fefa100018490ad4d304b7b7ab2658e3a4b80b1
Gordon Woodhull <gordon@woodhull.com>
2019-01-19 03:21:47 -0500

    add Object.values polyfill

--- src/core.js
+++ src/core.js
@@ -249,6 +249,34 @@ if (!Object.entries) {
   };
 }
 
+// https://github.com/KhaledElAnsari/Object.values
+Object.values = Object.values ? Object.values : function(obj) {
+    var allowedTypes = ["[object String]", "[object Object]", "[object Array]", "[object Function]"];
+    var objType = Object.prototype.toString.call(obj);
+
+    if(obj === null || typeof obj === "undefined") {
+	throw new TypeError("Cannot convert undefined or null to object");
+    } else if(!~allowedTypes.indexOf(objType)) {
+	return [];
+    } else {
+	// if ES6 is supported
+	if (Object.keys) {
+	    return Object.keys(obj).map(function (key) {
+		return obj[key];
+	    });
+	}
+
+	var result = [];
+	for (var prop in obj) {
+	    if (obj.hasOwnProperty(prop)) {
+		result.push(obj[prop]);
+	    }
+	}
+
+	return result;
+    }
+};
+
 function getBBoxNoThrow(elem) {
     // firefox seems to have issues with some of my texts
     // just catch for now
