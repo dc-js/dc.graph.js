d8570e7d338941c6e0e4e0ca3fad6ba3893e9c60
Gordon Woodhull <gordon@woodhull.com>
2019-03-01 15:06:04 -0500

    start filling in renderer and add enough code so it doesn't crash

--- src/render_webgl.js
+++ src/render_webgl.js
@@ -1,13 +1,45 @@
 dc_graph.render_webgl = function() {
     //var _svg = null, _defs = null, _g = null, _nodeLayer = null, _edgeLayer = null;
+    var _camera, _scene, _webgl_renderer;
+    var _controls;
+    var _nodeMaterial, _edgeMaterial;
     var _animating = false; // do not refresh during animations
-    //var _zoom;
     var _renderer = {};
-
-    _renderer.parent = property(null);
 
     _renderer.rendererType = function() {
         return 'webgl';
+    };
+
+    _renderer.parent = property(null);
+
+    _renderer.isRendered = function() {
+        return !!_camera;
+    };
+
+    _renderer.initializeDrawing = function () {
+        _camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        _camera.position.z = 1000;
+
+        _scene = new THREE.Scene();
+
+        _nodeMaterial =  new THREE.ShaderMaterial({
+            uniforms: {
+                color: { value: new THREE.Color('888888') }
+            },
+            vertexShader: document.getElementById( 'vertexshader' ).textContent,
+            fragmentShader: document.getElementById( 'fragmentshader' ).textContent
+        });
+        _edgeMaterial = new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 1 } );
+
+        _webgl_renderer = new THREE.WebGLRenderer({ antialias: true });
+        _webgl_renderer.setPixelRatio(window.devicePixelRatio);
+        _webgl_renderer.setSize(window.innerWidth, window.innerHeight);
+        _renderer.parent().root().node().appendChild(_webgl_renderer.domElement);
+
+        _controls = new THREE.OrbitControls(_camera, _webgl_renderer.domElement);
+        _controls.minDistance = 250;
+        _controls.maxDistance = 2000;
+        return _renderer;
     };
 
     _renderer.resize = function(w, h) {
@@ -39,32 +71,18 @@ dc_graph.render_webgl = function() {
     };
 
     _renderer.startRedraw = function(dispatch, wnodes, wedges) {
-        return drawState;
+        wnodes.forEach(infer_shape(_renderer.parent()));
+        return {wnodes: wnodes, wedges: wedges};
     };
-
-    _renderer.refresh = function(node, edge, edgeHover, edgeLabels, textPaths) {
-        if(_animating)
-            return _renderer; // but what about changed attributes?
-        return _renderer;
-    };
-
-    _renderer.reposition = function(node, edge) {
-        return _renderer;
-    };
-
-    function has_source_and_target(e) {
-        return !!e.source && !!e.target;
-    }
 
     _renderer.draw = function(drawState, animatePositions) {
-        return _renderer;
-    };
-
-    _renderer.isRendered = function() {
-        return !!_svg;
-    };
-
-    _renderer.initializeDrawing = function () {
+        drawState.wedges.forEach(function(e) {
+            if(!e.pos.old)
+                _renderer.parent().calcEdgePath(e, 'old', e.source.prevX || e.source.cola.x, e.source.prevY || e.source.cola.y,
+                                                e.target.prevX || e.target.cola.x, e.target.prevY || e.target.cola.y);
+            if(!e.pos.new)
+                _renderer.parent().calcEdgePath(e, 'new', e.source.cola.x, e.source.cola.y, e.target.cola.x, e.target.cola.y);
+        });
         return _renderer;
     };
 
@@ -85,14 +103,28 @@ dc_graph.render_webgl = function() {
     };
 
     _renderer.fireTSEvent = function(dispatch, drawState) {
-        dispatch.transitionsStarted(drawState.node, drawState.edge, drawState.edgeHover);
+        dispatch.transitionsStarted(null);
     };
 
     _renderer.calculateBounds = function(drawState) {
-        if(!drawState.node.size())
+        if(!drawState.wnodes.length)
             return null;
-        return _renderer.parent().calculateBounds(drawState.node.data(), drawState.edge.data());
+        return _renderer.parent().calculateBounds(drawState.wnodes, drawState.wedges);
     };
+
+    _renderer.refresh = function(node, edge, edgeHover, edgeLabels, textPaths) {
+        if(_animating)
+            return _renderer; // but what about changed attributes?
+        return _renderer;
+    };
+
+    _renderer.reposition = function(node, edge) {
+        return _renderer;
+    };
+
+    function has_source_and_target(e) {
+        return !!e.source && !!e.target;
+    }
 
     _renderer.animating = function() {
         return _animating;
