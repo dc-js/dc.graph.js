2b0e10ca90f2b201c53fc4256e8d0f65fa703f8b
Gordon Woodhull <gordon@woodhull.com>
2019-04-11 16:45:01 -0400

    artifacts

--- dc.graph.js
+++ dc.graph.js
@@ -1,5 +1,5 @@
 /*!
- *  dc.graph 0.9.0
+ *  dc.graph 0.8.11
  *  http://dc-js.github.io/dc.graph.js/
  *  Copyright 2015-2019 AT&T Intellectual Property & the dc.graph.js Developers
  *  https://github.com/dc-js/dc.graph.js/blob/master/AUTHORS
@@ -28,7 +28,7 @@
  * instance whenever it is appropriate.  The getter forms of functions do not participate in function
  * chaining because they return values that are not the diagram.
  * @namespace dc_graph
- * @version 0.9.0
+ * @version 0.8.11
  * @example
  * // Example chaining
  * diagram.width(600)
@@ -38,7 +38,7 @@
  */
 
 var dc_graph = {
-    version: '0.9.0',
+    version: '0.8.11',
     constants: {
         CHART_CLASS: 'dc-graph'
     }
