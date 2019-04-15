893ccd44ee4ab6129aa54f5d8894f2f4b0a7fb2b
Gordon Woodhull <gordon@woodhull.com>
2019-04-15 13:31:38 -0400

    don't crash evaluating whether to reject null mode

--- src/diagram.js
+++ src/diagram.js
@@ -1134,6 +1134,8 @@ dc_graph.diagram = function (parent, chartGroup) {
 
     _diagram.mode.reject = function(id, object) {
         var rtype = _diagram.renderer().rendererType();
+        if(!object)
+            return true; // null is always a valid mode for any renderer
         if(!object.supportsRenderer)
             console.log('could not check if "' + id + '" is compatible with ' + rtype);
         else if(!object.supportsRenderer(rtype))
