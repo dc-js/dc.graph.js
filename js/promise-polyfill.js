bdcb7caedb87378e818b4f7b3309050931ac38d4
Gordon Woodhull <gordon@woodhull.com>
2025-06-14 11:28:26 -0400

    Synced from from master branch.

--- js/promise-polyfill.js
+++ js/promise-polyfill.js
@@ -74,48 +74,6 @@ function allSettled(arr) {
   });
 }
 
-/**
- * @constructor
- */
-function AggregateError(errors, message) {
-  this.name = 'AggregateError', this.errors = errors;
-  this.message = message || '';
-}
-AggregateError.prototype = Error.prototype;
-
-function any(arr) {
-  var P = this;
-  return new P(function(resolve, reject) {
-    if (!(arr && typeof arr.length !== 'undefined')) {
-      return reject(new TypeError('Promise.any accepts an array'));
-    }
-
-    var args = Array.prototype.slice.call(arr);
-    if (args.length === 0) return reject();
-
-    var rejectionReasons = [];
-    for (var i = 0; i < args.length; i++) {
-      try {
-        P.resolve(args[i])
-          .then(resolve)
-          .catch(function(error) {
-            rejectionReasons.push(error);
-            if (rejectionReasons.length === args.length) {
-              reject(
-                new AggregateError(
-                  rejectionReasons,
-                  'All promises were rejected'
-                )
-              );
-            }
-          });
-      } catch (ex) {
-        reject(ex);
-      }
-    }
-  });
-}
-
 // Store setTimeout reference so promise-polyfill will be unaffected by
 // other code modifying setTimeout (like sinon.useFakeTimers())
 var setTimeoutFunc = setTimeout;
@@ -319,8 +277,6 @@ Promise.all = function(arr) {
   });
 };
 
-Promise.any = any;
-
 Promise.allSettled = allSettled;
 
 Promise.resolve = function(value) {
@@ -395,12 +351,9 @@ if (typeof globalNS['Promise'] !== 'function') {
 } else {
   if (!globalNS.Promise.prototype['finally']) {
     globalNS.Promise.prototype['finally'] = finallyConstructor;
-  }
+  } 
   if (!globalNS.Promise.allSettled) {
     globalNS.Promise.allSettled = allSettled;
-  }
-  if (!globalNS.Promise.any) {
-    globalNS.Promise.any = any;
   }
 }
 
