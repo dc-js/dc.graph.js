9d0d0c98cf185f3751853f15ba8960fc51e46e6a
Gordon Woodhull <gordon@woodhull.com>
2025-07-26 22:59:11 -0400

    remove artifacts and also d3(v3)-force

--- src/workers/d3-force-worker.js
+++
@@ -1,5 +0,0 @@
-// D3 Force layout web worker entry point
-import { d3ForceLayout } from '../d3_force_layout.js';
-import { createWorkerHandler } from './worker_common.js';
-
-onmessage = createWorkerHandler(d3ForceLayout);
