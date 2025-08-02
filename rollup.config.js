c135211168be94d7a388b7475460345d296e8c5f
Gordon Woodhull <gordon@woodhull.com>
2025-08-01 23:56:04 -0400

    claude: copy artifacts; more useful start command

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/index.js',
@@ -26,6 +27,13 @@ export default {
     resolve({
       preferBuiltins: false
     }),
-    commonjs()
+    commonjs(),
+    copy({
+      targets: [
+        { src: 'dist/dc-graph.js', dest: 'web/js' },
+        { src: 'dist/dc-graph.js.map', dest: 'web/js' }
+      ],
+      hook: 'writeBundle'
+    })
   ]
 };
